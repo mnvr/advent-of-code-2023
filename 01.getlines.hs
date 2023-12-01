d532bfd34d818a2a5b68867de72ca5e2cfeb2b98
Manav <manav@mrmr.io>
2023-12-01 22:29:56 +0530

    Remove acccidental prefix

--- 01.getlines.hs
+++ 01.getlines.hs
@@ -1,23 +1,23 @@
-    import System.IO.Error (tryIOError)
-    import Data.Char (isDigit, digitToInt)
-    import Data.List (isPrefixOf, findIndex)
+import System.IO.Error (tryIOError)
+import Data.Char (isDigit, digitToInt)
+import Data.List (isPrefixOf, findIndex)
 
-    main :: IO ()
-    main = getLines >>= (print . sum . fmap parse)
+main :: IO ()
+main = getLines >>= (print . sum . fmap parse)
 
-    getLines :: IO [String]
-    getLines = tryIOError getLine >>= e
-      where e (Left _) = pure []
-            e (Right s) = fmap (s :) getLines
+getLines :: IO [String]
+getLines = tryIOError getLine >>= e
+    where e (Left _) = pure []
+          e (Right s) = fmap (s :) getLines
 
-    parse :: String -> Int
-    parse s = first s spelled * 10 + first (reverse s) (map reverse spelled)
+parse :: String -> Int
+parse s = first s spelled * 10 + first (reverse s) (map reverse spelled)
 
-    first :: String -> [String] -> Int
-    first s@(c:s') ms = if isDigit c then digitToInt c else
-        case findIndex (`isPrefixOf` s) ms of
-            Nothing -> first s' ms
-            (Just i) -> i + 1
+first :: String -> [String] -> Int
+first s@(c:s') ms = if isDigit c then digitToInt c else
+    case findIndex (`isPrefixOf` s) ms of
+        Nothing -> first s' ms
+        (Just i) -> i + 1
 
-    spelled :: [String]
-    spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+spelled :: [String]
+spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
--- 01.hs
+++ 01.hs
@@ -1,17 +1,17 @@
-    import Data.Char (isDigit)
-    import Data.List (isPrefixOf, findIndex)
+import Data.Char (isDigit)
+import Data.List (isPrefixOf, findIndex)
 
-    main :: IO ()
-    main = interact $ (++ "\n") . show . sum . fmap parse . lines
+main :: IO ()
+main = interact $ (++ "\n") . show . sum . fmap parse . lines
 
-    parse :: String -> Int
-    parse s = first s spelled * 10 + first (reverse s) (map reverse spelled)
+parse :: String -> Int
+parse s = first s spelled * 10 + first (reverse s) (map reverse spelled)
 
-    first :: String -> [String] -> Int
-    first s@(c:s') ms = if isDigit c then read [c] else
-        case findIndex (`isPrefixOf` s) ms of
-            Nothing -> first s' ms
-            (Just i) -> i + 1
+first :: String -> [String] -> Int
+first s@(c:s') ms = if isDigit c then read [c] else
+    case findIndex (`isPrefixOf` s) ms of
+        Nothing -> first s' ms
+        (Just i) -> i + 1
 
-    spelled :: [String]
-    spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+spelled :: [String]
+spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
