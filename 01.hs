2d9d31174e3b58c52c291c67b739a3e21d4773ce
Manav <manav@mrmr.io>
2023-12-01 20:51:16 +0530

    Try prefix matching

--- 01.hs
+++ 01.hs
@@ -2,6 +2,7 @@ import System.IO.Error (tryIOError)
 import Data.Char (isDigit, digitToInt)
 import Data.Maybe (catMaybes, listToMaybe)
 import Data.Bifunctor (first)
+import Data.List (inits, find, isPrefixOf)
 
 main :: IO ()
 main = getLines >>= (print . sum . fmap parse)
@@ -12,41 +13,19 @@ getLines = tryIOError getLine >>= e
         e (Right s) = fmap (s :) getLines
 
 parse :: String -> Int
-parse s = head xs * 10 + head ys
-    where xs = catMaybes $ numbers s matchers
-          ys = catMaybes $ numbers (reverse s) reverseMatchers
+parse s = left s * 10 + right (reverse s)
 
-numbers :: String -> [Matcher] -> [Maybe Int]
-numbers [] _ = []
-numbers (c:s) ms = let (d, ms') = matchOrConsume c ms in d : numbers s ms'
+left :: String -> Int
+left s@(c:s') = if isDigit c then digitToInt c else
+    case find (\(p, i) -> p `isPrefixOf` s) (zip spelled [1..]) of
+        Nothing -> left s'
+        (Just (s, i)) -> i
 
--- A matcher is a map of strings to integers. When the string becomes empty,
--- we've "matched" the corresponding integer, otherwise the matcher should be
--- reset to the original state.
-data Matcher = Matcher { remaining :: String, num :: Int, original :: String }
-    deriving Show
-
--- 'matchOrConsume' takes a character and a current set of matchers, and if it
--- matches a digit based on the current set, it returns the digit. In either
--- case, it also returns the updated matchers.
-matchOrConsume :: Char -> [Matcher] -> (Maybe Int, [Matcher])
-matchOrConsume c ms
-    | isDigit c = (Just $ digitToInt c, ms)
-    | otherwise = first (listToMaybe . catMaybes) $ unzip $ map (`match` c) ms
-
-match :: Matcher -> Char -> (Maybe Int, Matcher)
-match m@(Matcher (r:rs) num original) c = (if isMatch then Just num else Nothing, m')
-  where m' = if null rs || c /= r then m { remaining = original } else m { remaining = rs }
-        isMatch = c == r && null rs
-
-matchers :: [Matcher]
-matchers = makeMatchers spelled
-
-reverseMatchers :: [Matcher]
-reverseMatchers = makeMatchers (map reverse spelled)
-
-makeMatchers :: [String] -> [Matcher]
-makeMatchers = zipWith (\ i s -> Matcher s i s) [1..]
+right :: String -> Int
+right s@(c:s') = if isDigit c then digitToInt c else
+    case find (\(p, i) -> p `isPrefixOf` s) (zip (map reverse spelled) [1..]) of
+        Nothing -> right s'
+        (Just (s, i)) -> i
 
 spelled :: [String]
 spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
