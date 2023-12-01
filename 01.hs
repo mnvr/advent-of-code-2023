f50fa93be76cd3337fddd2337953a96f04cc6c13
Manav <manav@mrmr.io>
2023-12-01 20:26:54 +0530

    Dedup

--- 01.hs
+++ 01.hs
@@ -23,7 +23,7 @@ numbers (c:s) ms = let (d, ms') = matchOrConsume c ms in d : numbers s ms'
 -- A matcher is a map of strings to integers. When the string becomes empty,
 -- we've "matched" the corresponding integer, otherwise the matcher should be
 -- reset to the original state.
-data Matcher = Matcher { remaining :: String, result :: Int, original :: String }
+data Matcher = Matcher { remaining :: String, num :: Int, original :: String }
     deriving Show
 
 -- 'matchOrConsume' takes a character and a current set of matchers, and if it
@@ -35,15 +35,18 @@ matchOrConsume c ms
     | otherwise = first (listToMaybe . catMaybes) $ unzip $ map (`match` c) ms
 
 match :: Matcher -> Char -> (Maybe Int, Matcher)
-match m@(Matcher (r:rs) result original) c = (if isMatch then Just result else Nothing, m')
+match m@(Matcher (r:rs) num original) c = (if isMatch then Just num else Nothing, m')
   where m' = if null rs || c /= r then m { remaining = original } else m { remaining = rs }
         isMatch = c == r && null rs
 
 matchers :: [Matcher]
-matchers = zipWith (\ s i -> Matcher s i s) spelled [1..]
+matchers = makeMatchers spelled
 
 reverseMatchers :: [Matcher]
-reverseMatchers = zipWith (\ s i -> Matcher s i s) (map reverse spelled) [1..]
+reverseMatchers = makeMatchers (map reverse spelled)
+
+makeMatchers :: [String] -> [Matcher]
+makeMatchers = zipWith (\ i s -> Matcher s i s) [1..]
 
 spelled :: [String]
 spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
