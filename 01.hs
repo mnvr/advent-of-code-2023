26b468ccfcbc73a8c5f7ca3a1d650c2ad4fdc8d4
Manav <manav@mrmr.io>
2023-12-01 18:41:27 +0530

    More

--- 01.hs
+++ 01.hs
@@ -1,6 +1,6 @@
 import System.IO.Error (tryIOError)
 import Data.Char (isDigit, digitToInt)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, listToMaybe)
 
 main :: IO ()
 main = getLines >>= (print . sum . fmap parse)
@@ -21,7 +21,7 @@ numbers (c:s) ms = let (d, ms') = matchOrConsume c ms in d : numbers s ms'
 -- A matcher is a map of strings to integers. When the string becomes empty,
 -- we've "matched" the corresponding integer, otherwise the matcher should be
 -- reset to the original state.
-data Matcher = Matcher { remaining :: String, result :: Integer, original :: String }
+data Matcher = Matcher { remaining :: String, result :: Int, original :: String }
 
 -- 'matchOrConsume' takes a character and a current set of matchers, and if it
 -- matches a digit based on the current set, it returns the digit. In either
@@ -29,7 +29,14 @@ data Matcher = Matcher { remaining :: String, result :: Integer, original :: Str
 matchOrConsume :: Char -> [Matcher] -> (Maybe Int, [Matcher])
 matchOrConsume c ms
     | isDigit c = (Just $ digitToInt c, ms)
-    | otherwise = (Nothing, ms)
+    | otherwise =
+        let (ds, ms') = unzip $ map (`match` c) ms in
+            -- ((listToMaybe . catMaybes) ds, ms')
+            (Nothing, ms')
+
+match :: Matcher -> Char -> (Maybe Int, Matcher)
+match (Matcher (r:rs) result original) c = if c == r then (Just result, m') else (Nothing, m')
+  where m' = if null rs then Matcher original result original else Matcher rs result original
 
 matchers :: [Matcher]
 matchers = zipWith (\ s i -> Matcher s i s) spelled [0..]
