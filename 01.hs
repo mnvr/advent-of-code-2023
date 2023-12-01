277fc26a07487d088a06318a670b29efa423859f
Manav <manav@mrmr.io>
2023-12-01 16:56:21 +0530

    Simplify

--- 01.hs
+++ 01.hs
@@ -1,18 +1,13 @@
-import GHC.IO (catchException, catch)
-import Control.Exception (IOException, try)
 import System.IO.Error (tryIOError)
-import Control.Monad (void)
 import Data.Char (isDigit, digitToInt)
 
 main :: IO ()
 main = parseLines parse >>= (print . sum)
 
 parseLines :: (String -> a) -> IO [a]
-parseLines f = do
-    es <- tryIOError getLine
-    case es of
-        (Left x) -> return []
-        (Right s) -> let x = f s in parseLines f >>= (\xs -> return (x : xs))
+parseLines f = tryIOError getLine >>= e
+  where e (Left _) = pure []
+        e (Right s) = parseLines f >>= (\xs -> pure (f s : xs))
 
 parse :: String -> Int
 parse s = head xs * 10 + last xs
