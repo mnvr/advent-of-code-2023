43df6c63684431989a2e2c16ff947d2e9d6aa3ca
Manav <manav@mrmr.io>
2023-12-01 16:19:18 +0530

    Digits

--- 01.hs
+++ 01.hs
@@ -2,6 +2,7 @@ import GHC.IO (catchException, catch)
 import Control.Exception (IOException, try)
 import System.IO.Error (tryIOError)
 import Control.Monad (void)
+import Data.Char (isDigit, digitToInt)
 
 main :: IO ()
 main = void $ tryIOError processLines
@@ -10,4 +11,7 @@ processLines :: IO ()
 processLines = processLine >> processLines
 
 processLine :: IO ()
-processLine = getLine >>= print
+processLine = getLine >>= (print . show . convert)
+
+-- convert :: String -> Integer
+convert s = map digitToInt $ filter isDigit s
