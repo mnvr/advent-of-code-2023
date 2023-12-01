891a4e946350e3858d373f9d2157949553a0f649
Manav <manav@mrmr.io>
2023-12-01 17:08:01 +0530

    Split parsing and processing

--- 01.hs
+++ 01.hs
@@ -1,14 +1,18 @@
 import System.IO.Error (tryIOError)
 import Data.Char (isDigit, digitToInt)
+import Data.Functor ((<&>))
 
 main :: IO ()
-main = parseLines parse >>= (print . sum)
+main = getLines >>= ((print . process) . fmap parse)
 
-parseLines :: (String -> a) -> IO [a]
-parseLines f = tryIOError getLine >>= e
+getLines :: IO [String]
+getLines = tryIOError getLine >>= e
   where e (Left _) = pure []
-        e (Right s) = parseLines f >>= (\xs -> pure (f s : xs))
+        e (Right s) = fmap (s :) getLines
 
 parse :: String -> Int
 parse s = head xs * 10 + last xs
     where xs = map digitToInt $ filter isDigit s
+
+process :: [Int] -> Int
+process = sum
