787473da52b3ac64fb7c2dd81aa8d521744cc8d2
Manav <manav@mrmr.io>
2023-12-01 17:50:34 +0530

    Doodle

--- 01.hs
+++ 01.hs
@@ -11,4 +11,27 @@ getLines = tryIOError getLine >>= e
 
 parse :: String -> Int
 parse s = head xs * 10 + last xs
-    where xs = map digitToInt $ filter isDigit s
+    where xs = numbers s matchers
+
+numbers :: String -> [Matcher] -> [Int]
+numbers [] _ = []
+numbers (c:s) ms = if isDigit c then digitToInt c : numbers s ms else numbers s ms
+
+-- A matcher is a map of strings to integers. When the string becomes empty,
+-- we've "matched" the corresponding integer, otherwise the matcher should be
+-- reset to the original state.
+data Matcher = Matcher { remaining :: String, result :: Integer, original :: String }
+
+-- 'matchOrConsume' takes a character and a current set of matchers, and if it
+-- matches a digit based on the current set, it returns the digit. In either
+-- case, it also returns the updated matchers.
+matchOrConsume :: Char -> [Matcher] -> (Maybe Int, [Matcher])
+matchOrConsume c ms
+    | isDigit c = (Just $ digitToInt c, ms)
+    | otherwise = (Nothing, ms)
+
+matchers :: [Matcher]
+matchers = zipWith (\ s i -> Matcher s i s) spelled [0..]
+
+spelled :: [String]
+spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
