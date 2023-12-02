486eb6a915e208674d1eb723a2220ce6c4cb91e1
Manav <manav@mrmr.io>
2023-12-02 17:24:07 +0530

    Part 2

--- 02.hs
+++ 02.hs
@@ -4,11 +4,18 @@
 import Data.Text qualified as T
 
 main :: IO ()
-main = interact $ (++"\n") . show . sum . map gid . p1 . map parse . lines
+main = interact $ (++"\n") . show . (\gs -> (p1 gs, p2 gs)) . map parse . lines
+-- main = debug1
 
-debug :: IO ()
-debug = interact $  unlines . concatMap each . take 10 . lines
-  where each l = let g = parse l in [l, show g, show $ length $ p1 [g]]
+debug1 :: IO ()
+debug1 = interact $  unlines . concatMap each . take 10 . lines
+  where each l = let g = parse l in
+          [l, show g, show . length $ filter possible [g]]
+
+debug2 :: IO ()
+debug2 = interact $  unlines . concatMap each . take 10 . lines
+  where each l = let g = parse l in
+          [l, show g, show $ fewest dzero g, show . power $ fewest dzero g]
 
 data Game = Game { gid :: Int, draws :: [Draw] } deriving Show
 data Draw = Draw { red :: Int, green :: Int, blue :: Int } deriving Show
@@ -20,9 +27,8 @@ parse s = Game { gid = gid, draws = draws }
         draws = map parseDraw $ T.split (==';') rt
 
 parseDraw :: T.Text -> Draw
-parseDraw t = foldl update zero iters
-    where zero = Draw { red = 0, green = 0, blue = 0 }
-          iters = T.split (==',') t
+parseDraw t = foldl update dzero iters
+    where iters = T.split (==',') t
           update d it = case color of
             "red" -> d { red = count }
             "green" -> d { green = count }
@@ -30,6 +36,9 @@ parseDraw t = foldl update zero iters
             where [count', color'] = T.words it
                   color = T.unpack color'
                   count = (read . T.unpack) count'
+
+dzero :: Draw
+dzero = Draw { red = 0, green = 0, blue = 0 }
 
 p1Threshold :: Draw
 p1Threshold = Draw { red = 12, green = 13, blue = 14 }
@@ -42,5 +51,20 @@ belowThreshold threshold draw = and [red draw <= red threshold,
 possible :: Game -> Bool
 possible = all (belowThreshold p1Threshold) . draws
 
-p1 :: [Game] -> [Game]
-p1 = filter possible
+p1 :: [Game] -> Int
+p1 = sum . map gid . filter possible
+
+power :: Draw -> Int
+power Draw { red = r, green = g, blue = b } = r * g * b
+
+fewest :: Draw -> Game -> Draw
+fewest d g = foldl m d (draws g)
+  where m d1 d2 = Draw { red = max (red d1) (red d2),
+                         green = max (green d1) (green d2),
+                         blue = max (blue d1) (blue d2) }
+
+powers :: [Game] -> [Int]
+powers = map (power . fewest dzero)
+
+p2 :: [Game] -> Int
+p2 = sum . powers
