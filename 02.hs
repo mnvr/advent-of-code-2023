92c97012c8d31691d2131664196fba5a7ddca536
Manav <manav@mrmr.io>
2023-12-02 23:50:46 +0530

    Make the parsec solution the primary one

--- 02.hs
+++ 02.hs
@@ -1,70 +1,56 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use &&" #-}
 
-import Data.Text qualified as T
+import Text.Parsec (string', char, digit, many1, eof, sepEndBy, sepBy, space, (<|>), parse, between, newline)
+import Text.Parsec.String (Parser)
+import Control.Exception (throw)
 
 main :: IO ()
-main = interact $ (++"\n") . show . (\gs -> (p1 gs, p2 gs)) . map parse . lines
--- main = debug1
-
-debug1 :: IO ()
-debug1 = interact $  unlines . concatMap each . take 10 . lines
-  where each l = let g = parse l in
-          [l, show g, show . length $ filter possible [g]]
-
-debug2 :: IO ()
-debug2 = interact $  unlines . concatMap each . take 10 . lines
-  where each l = let g = parse l in
-          [l, show g, show $ fewest dzero g, show . power $ fewest dzero g]
+main = interact $ (++"\n") . show . (\gs -> (p1 gs, p2 gs)) . parseGames
 
 data Game = Game { gid :: Int, draws :: [Draw] } deriving Show
 data Draw = Draw { red :: Int, green :: Int, blue :: Int } deriving Show
 
-parse :: String -> Game
-parse s = Game { gid = gid, draws = draws }
-  where [gt, rt] = T.split (==':') (T.pack s)
-        gid = read . T.unpack . last $ T.words gt
-        draws = map parseDraw $ T.split (==';') rt
+instance Semigroup Draw where
+  (Draw a b c) <> (Draw x y z) = Draw (a + x) (b + y) (c + z)
 
-parseDraw :: T.Text -> Draw
-parseDraw t = foldl update dzero iters
-    where iters = T.split (==',') t
-          update d it = case color of
-            "red" -> d { red = count }
-            "green" -> d { green = count }
-            "blue" -> d { blue = count }
-            where [count', color'] = T.words it
-                  color = T.unpack color'
-                  count = (read . T.unpack) count'
-
-dzero :: Draw
-dzero = Draw { red = 0, green = 0, blue = 0 }
-
-p1Threshold :: Draw
-p1Threshold = Draw { red = 12, green = 13, blue = 14 }
-
-belowThreshold :: Draw -> Draw -> Bool
-belowThreshold threshold draw = and [red draw <= red threshold,
-                                     green draw <= green threshold,
-                                     blue draw <= blue threshold]
+instance Monoid Draw where
+  mempty = Draw 0 0 0
 
 possible :: Game -> Bool
-possible = all (belowThreshold p1Threshold) . draws
+possible = all valid . draws
+  where valid (Draw r g b) = r <= 12 && g <= 13 && b <= 14
 
 p1 :: [Game] -> Int
 p1 = sum . map gid . filter possible
 
 power :: Draw -> Int
-power Draw { red = r, green = g, blue = b } = r * g * b
+power (Draw r g b) = r * g * b
 
-fewest :: Draw -> Game -> Draw
-fewest d g = foldl m d (draws g)
-  where m d1 d2 = Draw { red = max (red d1) (red d2),
-                         green = max (green d1) (green d2),
-                         blue = max (blue d1) (blue d2) }
+fewest :: [Draw] -> Draw
+fewest = foldl m mempty
+  where m (Draw a b c) (Draw x y z) = Draw (max a x) (max b y) (max c z)
 
 powers :: [Game] -> [Int]
-powers = map (power . fewest dzero)
+powers = map (power . fewest . draws)
 
 p2 :: [Game] -> Int
 p2 = sum . powers
+
+parseGames :: String -> [Game]
+parseGames s = case parse parser "" s of
+    Left err -> throw (userError (show err))
+    Right g -> g
+  where
+    parser = game `sepBy` newline <* eof
+    int = read <$> many1 digit
+    game = do
+      i <- between (string' "Game" >> space) (char ':') int
+      ds <- draws
+      return Game { gid = i, draws = ds }
+    draws = draw `sepBy` char ';'
+    draw = foldl (<>) mempty <$> (count `sepBy` char ',')
+    count = between space space int >>= \i ->
+      mempty { red = i } <$ string' "red" <|>
+      mempty { green = i } <$ string' "green" <|>
+      mempty { blue = i } <$ string' "blue"
---
+++ 02.split.hs
@@ -0,0 +1,64 @@
+import Data.Text qualified as T
+
+-- Variation of 02.hs that does manual line by line parsing using Data.Text
+-- instead of using Parsec.
+
+main :: IO ()
+main = interact $ (++"\n") . show . (\gs -> (p1 gs, p2 gs)) . map parse . lines
+-- main = debug1
+
+debug1 :: IO ()
+debug1 = interact $  unlines . concatMap each . take 10 . lines
+  where each l = let g = parse l in
+          [l, show g, show . length $ filter possible [g]]
+
+debug2 :: IO ()
+debug2 = interact $  unlines . concatMap each . take 10 . lines
+  where each l = let g = parse l in
+          [l, show g, show $ fewest (draws g), show . power $ fewest (draws g)]
+
+data Game = Game { gid :: Int, draws :: [Draw] } deriving Show
+data Draw = Draw { red :: Int, green :: Int, blue :: Int } deriving Show
+
+instance Semigroup Draw where
+  (Draw a b c) <> (Draw x y z) = Draw (a + x) (b + y) (c + z)
+
+instance Monoid Draw where
+  mempty = Draw 0 0 0
+
+possible :: Game -> Bool
+possible = all valid . draws
+  where valid (Draw r g b) = r <= 12 && g <= 13 && b <= 14
+
+p1 :: [Game] -> Int
+p1 = sum . map gid . filter possible
+
+power :: Draw -> Int
+power (Draw r g b) = r * g * b
+
+fewest :: [Draw] -> Draw
+fewest = foldl m mempty
+  where m (Draw a b c) (Draw x y z) = Draw (max a x) (max b y) (max c z)
+
+powers :: [Game] -> [Int]
+powers = map (power . fewest . draws)
+
+p2 :: [Game] -> Int
+p2 = sum . powers
+
+parse :: String -> Game
+parse s = Game { gid = gid, draws = draws }
+  where [gt, rt] = T.split (==':') (T.pack s)
+        gid = read . T.unpack . last $ T.words gt
+        draws = map parseDraw $ T.split (==';') rt
+
+parseDraw :: T.Text -> Draw
+parseDraw t = foldl update mempty iters
+    where iters = T.split (==',') t
+          update d it = case color of
+            "red" -> d { red = count }
+            "green" -> d { green = count }
+            "blue" -> d { blue = count }
+            where [count', color'] = T.words it
+                  color = T.unpack color'
+                  count = (read . T.unpack) count'
