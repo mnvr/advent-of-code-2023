4f7a3d386c4997f901ac6992f6b474c46508e5bb
Manav <manav@mrmr.io>
2023-12-02 15:42:14 +0530

    Read ids

--- 02.hs
+++ 02.hs
@@ -1,7 +1,16 @@
-main :: IO ()
-main = interact $ (++"\n") . show . map (gid . parse) . lines
+import Data.Text qualified as T
 
-data Game = Game { gid :: Int, draws :: [Int]}
+main :: IO ()
+-- main = interact $ (++"\n")
+main = interact $  unlines . concatMap (\l -> [l, (show . parse) l]) . lines
+-- main = interact $  unlines . map (id) . lines
+
+data Game = Game { gid :: Int, draws :: [Draw] } deriving Show
+type Draw = (Int,Int,Int)
 
 parse :: String -> Game
-parse s = Game { gid = 0, draws = []}
+parse s = Game { gid = gid, draws = draws }
+  where [gt, rt] = T.split (==':') (T.pack s)
+        gid = read . T.unpack . last $ T.words gt
+        draws = map draw $ T.split (==';') rt
+        draw t = (0,0,0)
