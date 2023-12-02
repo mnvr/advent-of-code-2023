93a5a8dfa1fa6624ef365e001dc9d016721da875
Manav <manav@mrmr.io>
2023-12-02 18:59:35 +0530

    Parse full input

--- 02.parsec.hs
+++ 02.parsec.hs
@@ -1,17 +1,29 @@
-import Text.Parsec (ParseError, digit, many1, parse)
+import Text.Parsec (ParseError, digit, many1, parse, sepBy, spaces, string, string', eof)
 import Text.Parsec.String (Parser)
 
 -- Parse a single integer
 integer :: Parser Integer
 integer = read <$> many1 digit
 
+-- Parse a list of integers separated by spaces
+integers :: Parser [Integer]
+integers = integer `sepBy` spaces
+
 -- Parse an integer from a string
-parseInteger :: String -> Either ParseError Integer
-parseInteger = parse integer ""
+parseInteger :: String -> Either ParseError [Integer]
+parseInteger = parse integers ""
+
+game :: Parser String
+game = string' "Game"
+
+myParser :: String -> Either ParseError ()
+myParser = parse parser ""
+  where parser = game >> eof
 
 main :: IO ()
 main = do
-  let input = "e123"
-  case parseInteger input of
+  -- let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+  let input = "Game"
+  case myParser input of
     Left err -> fail (show err)
     Right n -> putStrLn $ "Parsed: " ++ show n
