7680c97ec0b84324cec1bd4a659a85975be15f3c
Manav <manav@mrmr.io>
2023-12-02 13:39:58 +0530

    Simplify

--- 01.az.hs
+++ 01.az.hs
@@ -4,13 +4,12 @@ import Data.List (isPrefixOf, findIndex)
 -- Challenge: Use only 2 variables
 
 main :: IO ()
-main = interact $ (++ "\n") . show . sum . fmap parse . lines
+main = interact $ (++ "\n") . show . sum . map parse . lines
 
 parse :: String -> Int
-parse s = first s id * 10 + first (reverse s) reverse
+parse s = read ((\f -> first (f s) f) <$> [id, reverse] >>= id)
 
-first :: String -> (String -> String) -> Int
-first s f = if isDigit (head s) then read [head s] else
-    maybe (first (tail s) f) (+1) (findIndex (`isPrefixOf` s) (map f
+first :: String -> (String -> String) -> String
+first s f = if isDigit (head s) then take 1 s else
+    maybe (first (tail s) f) (show . (+1)) (findIndex (`isPrefixOf` s) (map f
              ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]))
-
