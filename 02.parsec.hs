b31b669bea79164f73c10f7f3d62245c86de3459
Manav <manav@mrmr.io>
2023-12-02 19:04:29 +0530

    Chain

--- 02.parsec.hs
+++ 02.parsec.hs
@@ -1,4 +1,4 @@
-import Text.Parsec (ParseError, digit, many1, parse, sepBy, spaces, string, string', eof)
+import Text.Parsec (ParseError, digit, many1, parse, sepBy, spaces, string, string', eof, space)
 import Text.Parsec.String (Parser)
 
 -- Parse a single integer
@@ -16,14 +16,14 @@ parseInteger = parse integers ""
 game :: Parser String
 game = string' "Game"
 
-myParser :: String -> Either ParseError ()
+myParser :: String -> Either ParseError Integer
 myParser = parse parser ""
-  where parser = game >> eof
+  where parser = game >> space >> integer >>= \i -> eof >> pure i
 
 main :: IO ()
 main = do
   -- let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-  let input = "Game"
+  let input = "Game 1"
   case myParser input of
     Left err -> fail (show err)
     Right n -> putStrLn $ "Parsed: " ++ show n
