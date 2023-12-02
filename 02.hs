83f52f8fb6ea3ab2728395c041946893726e83a0
Manav <manav@mrmr.io>
2023-12-02 16:12:59 +0530

    Parse draw

--- 02.hs
+++ 02.hs
@@ -2,15 +2,22 @@ import Data.Text qualified as T
 
 main :: IO ()
 -- main = interact $ (++"\n")
-main = interact $  unlines . concatMap (\l -> [l, (show . parse) l]) . lines
--- main = interact $  unlines . map (id) . lines
+main = interact $  unlines . concatMap (\l -> [l, (show . parse) l]) . take 1 . lines
 
 data Game = Game { gid :: Int, draws :: [Draw] } deriving Show
-type Draw = (Int,Int,Int)
+data Draw = Draw { red :: Int, green :: Int, blue :: Int } deriving Show
 
 parse :: String -> Game
 parse s = Game { gid = gid, draws = draws }
   where [gt, rt] = T.split (==':') (T.pack s)
         gid = read . T.unpack . last $ T.words gt
-        draws = map draw $ T.split (==';') rt
-        draw t = (0,0,0)
+        draws = map parseDraw $ T.split (==';') rt
+
+parseDraw :: T.Text -> Draw
+parseDraw t = foldl update zero iters
+    where zero = Draw { red = 0, green = 0, blue = 0 }
+          iters = T.split (==',') t
+          update d it = let [count, color] = T.words it in case T.unpack color of
+            "red" -> d { red = (read . T.unpack) count }
+            "green" -> d { green = (read . T.unpack) count }
+            "blue" -> d { blue = (read . T.unpack) count }
