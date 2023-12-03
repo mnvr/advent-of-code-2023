32d2f2d2eb6af9cc7fa7b6d2a7c5e939fcebd340
Manav <manav@mrmr.io>
2023-12-03 15:48:08 +0530

    More massaging

--- 03.hs
+++ 03.hs
@@ -8,8 +8,8 @@ main = interact $ (++ "\n") . show . p1
 data Grid = Grid { rows :: [String], my :: Int, mx :: Int }
   deriving Show
 
-data Part = Part { pc :: Char, symbols :: [Cell] }
-data Cell = Cell { c :: Char, y :: Int, x :: Int }
+data PartDigit = Part { pdChar :: Char, pdNeighbourSymbols :: [Cell] }
+data Cell = Cell { cc :: Char, cy :: Int, cx :: Int } deriving Eq
 
 makeGrid :: String -> Grid
 makeGrid s = Grid { rows = ls, my = length ls - 1, mx = length (head ls) - 1 }
@@ -22,7 +22,8 @@ neighbouringCells grid y x = catMaybes
 
 cell :: Grid -> Int -> Int -> Maybe Cell
 cell grid y x
-  | isInBounds grid y x = Just Cell { c = (rows grid !! y) !! x, y = y, x = x }
+  | isInBounds grid y x = Just Cell {
+                            cc = (rows grid !! y) !! x, cy = y, cx = x }
   | otherwise = Nothing
 
 isInBounds :: Grid -> Int -> Int -> Bool
@@ -35,25 +36,27 @@ nearSymbol :: Grid -> Int -> Int -> Bool
 nearSymbol grid y x = not . null $ neighbouringSymbols grid y x
 
 isSymbol :: Cell -> Bool
-isSymbol (Cell {c}) = (not . isDigit) c && c /= '.'
+isSymbol (Cell {cc}) = (not . isDigit) cc && cc /= '.'
 
 -- A particular index is a digit of a part number if (a) it is a digit, and (b)
 -- if any of the digits of that number are near a symbol.
-digitOfPart :: Grid -> Int -> Int -> Maybe Char
-digitOfPart grid y x = cell grid y x >>= \(Cell {c}) ->
-    c <$ digitOfPart_ [] grid y x
+partDigit :: Grid -> Int -> Int -> Maybe Char
+partDigit grid y x = cell grid y x >>= \(Cell {cc}) ->
+    cc <$ neighbouringSymbolsOfPartDigit_ [] grid y x
 
-digitOfPart_ :: [(Int,Int)] -> Grid -> Int -> Int -> Maybe [Cell]
-digitOfPart_ seen grid y x
+neighbouringSymbolsOfPartDigit_ ::
+    [(Int,Int)] -> Grid -> Int -> Int -> Maybe [Cell]
+neighbouringSymbolsOfPartDigit_ seen grid y x
     | (y,x) `elem` seen = Nothing
     | not $ isInBounds grid y x = Nothing
     | otherwise =
         let c = rows grid !! y !! x
         in if isDigit c
            then let ns = neighbouringSymbols grid y x
+                    seen' = ((y,x) : seen)
                 in if null ns
-                   then digitOfPart_ ((y,x) : seen) grid y (x - 1)
-                    <|> digitOfPart_ ((y,x) : seen) grid y (x + 1)
+                   then neighbouringSymbolsOfPartDigit_ seen' grid y (x - 1)
+                    <|> neighbouringSymbolsOfPartDigit_ seen' grid y (x + 1)
                    else Just ns
            else Nothing
 
@@ -64,7 +67,7 @@ validDigits = concatMap numbers . validDigitsOrSpaces
 validDigitsOrSpaces :: Grid -> [String]
 validDigitsOrSpaces grid = [partDigitsInRow y | y <- [0..my grid]]
   where partDigitsInRow y = [partDigitOrSpace y x | x <- [0..mx grid]]
-        partDigitOrSpace y x = fromMaybe ' ' $ digitOfPart grid y x
+        partDigitOrSpace y x = fromMaybe ' ' $ partDigit grid y x
 
 partNumbers :: String -> [Int]
 partNumbers = validDigits . makeGrid
