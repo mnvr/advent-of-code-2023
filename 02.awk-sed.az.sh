dfb2add9b211fb0bb413aad944553b0c2d9632cc
Manav <manav@mrmr.io>
2023-12-03 10:23:21 +0530

    Parse games

--- 02.awk-sed.az.sh
+++ 02.awk-sed.az.sh
@@ -3,7 +3,12 @@
 # What do unix aficianados use to slice and dice in the kitchen? Awk and sed
 # indeed!
 
-cat examples/02 | head -2 | tr ",;:" '\n' | \
-#   awk '/Game/ { if (NR != 1) print $r, $g, $b; r=0; g=0; b=0; } '
-#   awk '/Game/ { if (NR > 1) print NR }'
-  awk '/Game/ { if ($2 > 1) print r, g, b; r=0;g=0;b=0 }'
+test -z "$1" && echo "usage: $0 <path-to-input>" && exit 1
+
+cat "$1" | tr ",;:" '\n' | \
+  awk '
+  /Game/ { if ($2 > 1) print r, g, b; r=0; g=0; b=0 }
+  /red/   { if (r < $1) r = $1 }
+  /green/ { if (g < $1) g = $1 }
+  /blue/  { if (b < $1) b = $1 }
+  '
