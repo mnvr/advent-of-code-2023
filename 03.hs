b9d1b6bef053716b4f301258e180910898dacaca
Manav <manav@mrmr.io>
2023-12-03 15:18:52 +0530

    Prepare for part 2

--- 03.hs
+++ 03.hs
@@ -1,5 +1,6 @@
 import Data.Char (isDigit)
 import Data.Maybe (catMaybes, fromMaybe)
+import Control.Applicative ((<|>))
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1
@@ -7,44 +8,54 @@ main = interact $ (++ "\n") . show . p1
 data Grid = Grid { rows :: [String], my :: Int, mx :: Int }
   deriving Show
 
+data Part = Part { pc :: Char, symbols :: [Cell] }
+data Cell = Cell { c :: Char, y :: Int, x :: Int }
+
 makeGrid :: String -> Grid
 makeGrid s = Grid { rows = ls, my = length ls - 1, mx = length (head ls) - 1 }
     where ls = lines s
 
-neighbouringCells :: Grid -> Int -> Int -> [Char]
+neighbouringCells :: Grid -> Int -> Int -> [Cell]
 neighbouringCells grid y x = catMaybes
     [cell grid (y + u) (x + v) | u <- [-1,0,1], v <- [-1,0,1],
                                  not (u == 0 && v == 0)]
 
-cell :: Grid -> Int -> Int -> Maybe Char
+cell :: Grid -> Int -> Int -> Maybe Cell
 cell grid y x
-  | isInBounds grid y x = Just ((rows grid !! y) !! x)
+  | isInBounds grid y x = Just Cell { c = (rows grid !! y) !! x, y = y, x = x }
   | otherwise = Nothing
 
 isInBounds :: Grid -> Int -> Int -> Bool
 isInBounds (Grid {my, mx}) y x = y >= 0 && y <= my && x >= 0 && x <= mx
 
-nearSymbol :: Grid -> Int -> Int -> Bool
-nearSymbol grid y x = any isSymbol (neighbouringCells grid y x)
+neighbouringSymbols :: Grid -> Int -> Int -> [Cell]
+neighbouringSymbols grid y x = filter isSymbol (neighbouringCells grid y x)
 
-isSymbol :: Char -> Bool
-isSymbol c = (not . isDigit) c && c /= '.'
+nearSymbol :: Grid -> Int -> Int -> Bool
+nearSymbol grid y x = not . null $ neighbouringSymbols grid y x
+
+isSymbol :: Cell -> Bool
+isSymbol (Cell {c}) = (not . isDigit) c && c /= '.'
 
 -- A particular index is a digit of a part number if (a) it is a digit, and (b)
 -- if any of the digits of that number are near a symbol.
 digitOfPart :: Grid -> Int -> Int -> Maybe Char
-digitOfPart grid y x = cell grid y x >>= \c ->
-    if digitOfPart_ [] grid y x then Just c else Nothing
+digitOfPart grid y x = cell grid y x >>= \(Cell {c}) ->
+    c <$ digitOfPart_ [] grid y x
 
-digitOfPart_ :: [(Int,Int)] -> Grid -> Int -> Int -> Bool
+digitOfPart_ :: [(Int,Int)] -> Grid -> Int -> Int -> Maybe [Cell]
 digitOfPart_ seen grid y x
-    | (y,x) `elem` seen = False
-    | not $ isInBounds grid y x = False
+    | (y,x) `elem` seen = Nothing
+    | not $ isInBounds grid y x = Nothing
     | otherwise =
         let c = rows grid !! y !! x
-        in (isDigit c && (nearSymbol grid y x
-                || digitOfPart_ ((y,x) : seen) grid y (x - 1)
-                || digitOfPart_ ((y,x) : seen) grid y (x + 1)))
+        in if isDigit c
+           then let ns = neighbouringSymbols grid y x
+                in if null ns
+                   then digitOfPart_ ((y,x) : seen) grid y (x - 1)
+                    <|> digitOfPart_ ((y,x) : seen) grid y (x + 1)
+                   else Just ns
+           else Nothing
 
 validDigits :: Grid -> [Int]
 validDigits = concatMap numbers . validDigitsOrSpaces
