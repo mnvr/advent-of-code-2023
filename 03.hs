e22a932c4e3e215a3c51d157217bb6926c809f5d
Manav <manav@mrmr.io>
2023-12-03 12:21:22 +0530

    Nested chunks

--- 03.hs
+++ 03.hs
@@ -1,10 +1,8 @@
 main :: IO ()
 main = interact $ (++"\n") . show . parse
 
-parse = chunks
-
--- Overlapping chunks / neighbourhoods of 3
-chunks :: String -> [(String, String, String)]
-chunks s = zip3 (blank : ls) ls (tail ls ++ [blank])
-  where ls = take 4 . lines $ s
+parse s = chunks
+  where chunks = zipWith3 zChars (blank : ls) ls (tail ls ++ [blank])
+        ls = take 4 . lines $ s
         blank = replicate (length $ head ls) '.'
+        zChars = zipWith3 (\c1 c2 c3 -> [c1, c2, c3])
