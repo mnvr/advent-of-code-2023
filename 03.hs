d0269283af99de518acbb56a18f7740e0c5107de
Manav <manav@mrmr.io>
2023-12-03 12:03:50 +0530

    Split into chunks of 3

--- 03.hs
+++ 03.hs
@@ -1,2 +1,10 @@
 main :: IO ()
-main = interact $ id
+main = interact $ (++"\n") . show . parse
+
+parse = chunks
+
+-- Overlapping chunks / neighbourhoods of 3
+chunks :: String -> [(String, String, String)]
+chunks s = zip3 (blank : ls) ls (tail ls ++ [blank])
+  where ls = take 4 . lines $ s
+        blank = replicate (length $ head ls) '.'
