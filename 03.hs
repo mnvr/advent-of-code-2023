2ec1b280106b8343aa1ea1328701ca7e20e6bc15
Manav <manav@mrmr.io>
2023-12-03 12:55:22 +0530

    Grid

--- 03.hs
+++ 03.hs
@@ -1,8 +1,28 @@
+import Data.Char (isDigit)
+import Data.Maybe (catMaybes)
+
 main :: IO ()
 main = interact $ (++"\n") . show . parse
 
-parse s = chunks
-  where chunks = zipWith3 zChars (blank : ls) ls (tail ls ++ [blank])
-        ls = take 4 . lines $ s
-        blank = replicate (length $ head ls) '.'
-        zChars = zipWith3 (\c1 c2 c3 -> [c1, c2, c3])
+data Grid = Grid { rows :: [String], rowCount :: Int, colCount :: Int }
+  deriving Show
+
+makeGrid :: String -> Grid
+makeGrid s = Grid { rows = ls, rowCount = length ls, colCount = length (head ls) }
+    where ls = take 4 . drop 0 . lines $ s
+
+isSymbol :: Char -> Bool
+isSymbol c = (not . isDigit) c && c /= '.'
+
+neighbouringCells :: Grid -> Int -> Int -> [Char]
+neighbouringCells grid y x = catMaybes
+    [cell grid (y + u) (x + v) | u <- [-1,0,1], v <- [-1,0,1], not (u == 0 && v == 0)]
+
+cell :: Grid -> Int -> Int -> Maybe Char
+cell (Grid {rows, rowCount, colCount}) y x
+  | y >= 0 && y < rowCount && x >= 0 && x < colCount = Just ((rows !! y) !! x)
+  | otherwise = Nothing
+
+parse s = test
+  where grid = makeGrid s
+        test = neighbouringCells grid 0 2
