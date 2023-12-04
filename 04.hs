4d9e860b14dce3c7b4e4e1742a8bcdc958561870
Manav <manav@mrmr.io>
2023-12-04 13:43:47 +0530

    More cards

--- 04.hs
+++ 04.hs
@@ -6,21 +6,15 @@ main = interact $ (++ "\n") . show . parseCards
 
 data Card = Card { winning :: [Int], have :: [Int] } deriving (Show)
 
--- parseCards :: String -> [Card]
-parseCards s = case parse cards "" (head (lines s)) of
+parseCards :: String -> [Card]
+parseCards s = case parse cards "" s of
     Left err -> error (show err)
     Right g -> g
   where
-    cards = card -- `sepBy` newline
+    cards = many1 card
     card = do
-        n1 <- string "Card" *> space *> num *> char ':' *> nums <* char '|'
-        n2 <- nums <* eof
-        pure (n1, n2)
-    -- <*> nums <* eof
-    num :: Parser Int
+        winning <- prelude *> nums <* char '|'
+        Card winning <$> nums
+    prelude = string "Card" *> space *> num *> char ':'
     num = read <$> many1 digit
     nums = many1 (between (many space) (many space) num)
-    -- winning = nums `endBy` string "|"
-    -- winning = manyTill (num <* space) eof -- (char '|')
-    -- nums = num `sepBy` (many space) -- many1 (space *> num) `endBy` (space *> char '|')
-    rest = many1 (space *> num)
