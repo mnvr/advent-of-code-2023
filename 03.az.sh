21b58fdd5d7f416dfaa603b72ad7e474e349367e
Manav <manav@mrmr.io>
2023-12-04 10:43:59 +0530

    Find column

--- 03.az.sh
+++ 03.az.sh
@@ -11,3 +11,11 @@ cat "$1" | head -2 | tr -s '.' ' ' | awk '
   { for (i=1; i<=NF; i++) if ($i ~ /^[0-9]+$/) print "Number " $i " on row " NR  }
   { for (i=1; i<=NF; i++) if ($i !~ /^[0-9]+$/) print "Symbol " $i " on row " NR  }
 ' | tee /tmp/$0.tmp.1
+
+cat /tmp/$0.tmp.1 | awk '/Number/ { print $2, $5 }' | while read n r
+do
+  echo "Looking for $n from row $r"
+  cat "$1" | grep --word $n | awk -vn=$n -vr=n '
+    { print "Number " n " on row " r " and column " index($0, n) }
+  ' | tee /tmp/$0.tmp.2
+done
