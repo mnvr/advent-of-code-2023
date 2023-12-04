39be2743c59398a69e2bd3d7bb21499f179d18d0
Manav <manav@mrmr.io>
2023-12-04 13:17:49 +0530

    Seeds of a parser

--- 04.hs
+++ 04.hs
@@ -7,9 +7,15 @@ main = interact $ (++ "\n") . show . parseCards
 data Card = Card { winning :: [Int], have :: [Int] } deriving (Show)
 
 -- parseCards :: String -> [Card]
-parseCards s = case parse cards "" s of
+parseCards s = case parse cards "" (head (lines s)) of
     Left err -> error (show err)
     Right g -> g
   where
-    cards = card `sepBy` newline
-    card = string "Card"
+    cards = card -- `sepBy` newline
+    card = string "Card" *> space *> num *> char ':' *> space *> nums
+    num :: Parser Int
+    num = read <$> many1 digit
+    nums = num `sepBy` space
+    winning = winning2 `endBy` string "|"
+    winning2 = num `sepBy` space -- many1 (space *> num) `endBy` (space *> char '|')
+    rest = many1 (space *> num)
