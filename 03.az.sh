594bbd39495ca8731b2c2197ac8973682a094c05
Manav <manav@mrmr.io>
2023-12-04 10:54:37 +0530

    Symbols too

--- 03.az.sh
+++ 03.az.sh
@@ -15,7 +15,15 @@ cat "$1" | head -2 | tr -s '.' ' ' | awk '
 cat /tmp/$0.tmp.1 | awk '/Number/ { print $2, $5 }' | while read n r
 do
   echo "Looking for $n from row $r"
-  cat "$1" | grep --word $n | awk -vn=$n -vr=n '
+  cat "$1" | grep --word $n | awk -vn=$n -vr=r '
     { print "Number " n " on row " r " and column " index($0, n) }
   ' | tee /tmp/$0.tmp.2
+done
+
+cat /tmp/$0.tmp.1 | awk '/Symbol */ { print $5 }' | while read r
+do
+  echo "Looking for * from row $r"
+  cat "$1" | awk -vr=2 '
+    NR == r { print "Symbol * on row " r " and column " index($0, "*") }
+  ' | tee /tmp/$0.tmp.3
 done
