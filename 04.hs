7407bff1e43c4c9ae3c7d132b325094b878cecff
Manav <manav@mrmr.io>
2023-12-04 13:58:40 +0530

    p1 example

--- 04.hs
+++ 04.hs
@@ -2,7 +2,7 @@ import Text.Parsec
 import Text.Parsec.String (Parser)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . parseCards
+main = interact $ (++ "\n") . show . p1 . parseCards
 
 data Card = Card { winning :: [Int], have :: [Int] } deriving (Show)
 
@@ -16,3 +16,11 @@ parseCards s = case parse cards "" s of
     prelude = string "Card" *> space *> num *> char ':'
     num = read <$> many1 digit
     nums = many1 (between (many space) (many space) num)
+
+points :: Card -> Int
+points Card { winning, have } = pt . length $ filter (`elem` have) winning
+  where pt 0 = 0
+        pt n = 2 ^ (n - 1)
+
+p1 :: [Card] -> Int
+p1 = sum . map points
