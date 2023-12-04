9c9c433019d1ca48ac8200accc8ec36856bae851
Manav <manav@mrmr.io>
2023-12-04 13:31:28 +0530

    Remove conflicting parses

--- 04.hs
+++ 04.hs
@@ -12,10 +12,11 @@ parseCards s = case parse cards "" (head (lines s)) of
     Right g -> g
   where
     cards = card -- `sepBy` newline
-    card = string "Card" *> space *> num *> char ':' *> space *> nums
+    card = string "Card" *> space *> num *> char ':' *> space *> nums <* char '|'
     num :: Parser Int
     num = read <$> many1 digit
-    nums = num `sepBy` space
-    winning = winning2 `endBy` string "|"
+    nums = many1 (num <* space)
+    -- winning = nums `endBy` string "|"
+    -- winning = manyTill (num <* space) eof -- (char '|')
     winning2 = num `sepBy` space -- many1 (space *> num) `endBy` (space *> char '|')
     rest = many1 (space *> num)
