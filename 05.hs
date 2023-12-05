d1fbe0525f7a5b197359e76ad4bab7359e63b7eb
Manav <manav@mrmr.io>
2023-12-05 21:08:48 +0530

    Tinker with the parser
    This is a great tutorial!
    https://jsdw.me/posts/haskell-parsec-basics/

--- 05.hs
+++ 05.hs
@@ -11,6 +11,45 @@ data Map = Map { sourceRange :: Int, destinationRange :: Int, length :: Int } de
 parseAlmanac s = case parse almanac "" s of
     (Left err)-> error (show err)
     (Right v) -> v
-  where almanac = string "seeds"
+  where
+     sp :: Parser Char = char ' '
+     num :: Parser Int
+     num = read <$> many1 digit
+     nums :: Parser [Int]
+     nums = sepBy num sp
+     almanac = do
+        string "seeds: "
+        seeds <- nums
+        newline
+        newline
+        string "seed-to-soil map:"
+        newline
+        range1 <- nums
+        newline
+        range2 <- nums
+        newline
+        newline
+        string "soil-to-fertilizer map:"
+        newline
+        range3 <- nums
+        newline
+        range4 <- nums
+        newline
+        range5 <- nums
+        newline
+        pure (seeds, [[range1, range2], [range3, range4, range5]])
+
+
+        -- seeds = (,) <$> ( *> nums <* count 2 newline) <*> maps
+        -- num :: Parser Int
+        -- num = read <$> many1 digit
+        -- nums = many1 (between sp sp num) --`sepBy` spaces
+        -- maps = (,) <$> mhead <*> range
+        -- mhead = manyTill (noneOf "\n") endOfLine
+        -- ranges = count 2 range -- range `sepBy` newline
+        -- range = count 3 nums
+        -- sp = many (char ' ')
+
+
 
 p1 = id
