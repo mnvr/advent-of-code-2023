7cb5f0c08c154998612e2a739290793105077e0e
Manav <manav@mrmr.io>
2023-12-05 07:38:30 +0530

    Extract

--- 03.az.sh
+++ 03.az.sh
@@ -7,26 +7,26 @@ test -z "$1" && echo "usage: $0 <path-to-input>" && exit 1
 
 echo "Hello Dear May," | tee /tmp/ac3.facts
 
+read_log () { cp /tmp/ac3.facts /tmp/ac3.facts.old; cat /tmp/ac3.facts.old; }
+log () { tee -a /tmp/ac3.facts; }
+
 cat "$1" | awk -F '[^0-9]' '
   { for (i=1; i<=NF; i++) { if ($i != "") print "Number " $i " on row " NR; } }
-' | tee -a /tmp/ac3.facts
+' | log
 
 cat "$1" | tr '.' ' ' | tr -d '0-9' | awk '
   { for (i=1; i<=NF; i++) { if ($i != "") print "Symbol " $i " on row " NR; } }
-' | tee -a /tmp/ac3.facts
+' | log
 
-cp /tmp/ac3.facts /tmp/ac3.facts.old
-cat /tmp/ac3.facts.old | awk '/Number/ { print $2, $5 }' | while read n r
+read_log | awk '/Number/ { print $2, $5 }' | while read n r
 do
   echo "Looking for $n from row $r"
   cat "$1" | awk -vn=$n -vr=$r '
     NR == r { print "Number " n " on row " r " and column " index($0, n) }
-  ' | tee -a /tmp/ac3.facts
+  ' | log
 done
 
-cp /tmp/ac3.facts /tmp/ac3.facts.old
-cat /tmp/ac3.facts.old | awk '/Symbol */ { print $5 }' |
-while read r
+read_log | awk '/Symbol */ { print $5 }' | while read r
 do
   echo "Looking for * from row $r"
   cat "$1" | awk -vr=$r '
@@ -37,11 +37,10 @@ do
       print "Symbol * on row " r " and column " c;
       s = substr(s, c + 1);
     } }
-  ' | tee -a /tmp/ac3.facts
+  ' | log
 done
 
-cp /tmp/ac3.facts /tmp/ac3.facts.old
-cat /tmp/ac3.facts.old | grep column | awk '/Number/ { print $2, $5, $8 }' |
+read_log | grep column | awk '/Number/ { print $2, $5, $8 }' |
 while read n r c
 do
   nsym=$(cat "$1" | awk -vn=$n -vr=$r -vc=$c '
@@ -50,18 +49,17 @@ do
     }
   ' | tr -d '0-9.\n' | wc -c | tr -d ' ')
   echo "Number $n on row $r and column $c has $nsym symbols around it" \
-    | tee -a /tmp/ac3.facts
+    | log
   if test "$nsym" -ne 0
   then
     echo "Part $n on row $r and column $c" \
-    | tee -a /tmp/ac3.facts
+    | log
   fi
 done
 
-cp /tmp/ac3.facts /tmp/ac3.facts.old
-cat /tmp/ac3.facts.old | awk '/Part/ { s += $2 }
-  END { print "The sum of all part numbers is " s }' | tee -a /tmp/ac3.facts
+read_log | awk '/Part/ { s += $2 }
+  END { print "The sum of all part numbers is " s }' | log
 
 rm /tmp/ac3.facts.old   #  One must keep flowing, like water.
 
-echo "Yours Truly," | tee -a /tmp/ac3.facts
+echo "Yours Truly," | log
