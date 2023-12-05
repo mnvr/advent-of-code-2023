ca19f8222e20f9310ff243517dcf73b40bea2ddd
Manav <manav@mrmr.io>
2023-12-05 21:17:13 +0530

    T2

--- 05.hs
+++ 05.hs
@@ -9,20 +9,24 @@ data Map = Map { sourceRange :: Int, destinationRange :: Int, length :: Int } de
 
 -- parseAlmanac :: String -> Input
 parseAlmanac s = case parse almanac "" s of
-    (Left err)-> error (show err)
-    (Right v) -> v
+    Left err -> error (show err)
+    Right v -> v
   where
      sp :: Parser Char = char ' '
      num :: Parser Int
      num = read <$> many1 digit
      nums :: Parser [Int]
      nums = sepBy num sp
+     range :: Parser [Int]
+     range = do
+        n1 <- num
+        sp
+        n2 <- num
+        sp
+        n3 <- num
+        pure [n1, n2, n3]
      _ranges1 = do
-        range1 <- nums
-        newline
-        range2 <- nums
-        newline
-        return [range1, range2]
+        many1 (range >>= \ns -> newline >> pure ns)
      almanac = do
         string "seeds: "
         seeds <- nums
@@ -31,19 +35,26 @@ parseAlmanac s = case parse almanac "" s of
         string "seed-to-soil map:"
         newline
         ranges1 <- _ranges1
+        newline
+        -- string "soil-to-fertilizer map:"
+        -- ranges2 <- _ranges1
         -- newline
+
         -- range2 <- nums
         -- newline
-        newline
-        string "soil-to-fertilizer map:"
-        newline
-        range3 <- nums
-        newline
-        range4 <- nums
-        newline
-        range5 <- nums
-        newline
-        pure (seeds, [ranges1, [range3, range4, range5]])
+        -- newline
+        -- string "soil-to-fertilizer map:"
+        -- newline
+        -- range3 <- nums
+        -- newline
+        -- range4 <- nums
+        -- newline
+        -- range5 <- nums
+        -- newline
+        pure (seeds, [ranges1])
+        --,
+        --[range3, range4, range5]
+        -- ])
 
 
         -- seeds = (,) <$> ( *> nums <* count 2 newline) <*> maps
