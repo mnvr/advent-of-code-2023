438fd6ec3667ab7be864d6f924ef747b348c46cc
Manav <manav@mrmr.io>
2023-12-06 16:36:24 +0530

    Parse input

--- 06.hs
+++ 06.hs
@@ -3,13 +3,16 @@ import Text.Parsec
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parseRaces
 
-data Races = Races { times:: [Int], distance :: [Int] }
+data Races = Races { times:: [Int], distance :: [Int] } deriving Show
 
-parseRaces :: String -> Races
+-- parseRaces :: String -> Races
 parseRaces s = case parse races "" s of
     Left e -> error (show e)
     Right v -> v
-  where races = string "Time"
+  where races = Races <$> (nums <* newline) <*> nums
+        num = read <$> many1 digit
+        nums = skipMany notDigitOrNL *> many1 (num <* skipMany notDigitOrNL)
+        notDigitOrNL = noneOf ('\n' : ['0'..'9'])
 
 -- p1 :: Races -> Int
 p1 = id
