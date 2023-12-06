356363c560a6cfe3d9ca1c5938b97dc32348247b
Manav <manav@mrmr.io>
2023-12-06 14:31:35 +0530

    Use an AWK associative array and exponentiation
    Reference:
    https://stackoverflow.com/questions/15418547/how-does-associative-arrays-work-in-awk

--- 04.az.sh
+++ 04.az.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # 2**(len(line.split()) - len(set(line.split())) - 1)
-cat examples/04 | head -1 | tr -d '|' | awk '{ for(i=3;i<=NF;i++) print $i+0; }'
+cat examples/04 | head -1 | tr -d '|' | awk '{ for(i=3;i<=NF;i++) print cs[$i]+=1; } END { for(c in cs) print c; for(c in cs) if (cs[c]>1) {s+=1; print cs[c], s;}; print (2 ^ (s - 1)) }'
