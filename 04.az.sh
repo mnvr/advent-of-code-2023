abb0e5ca7baf81613021027427e7a7dbcfdbc554
Manav <manav@mrmr.io>
2023-12-06 14:41:06 +0530

    D4 Part 1 in awk

--- 04.az.sh
+++ 04.az.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-# 2**(len(line.split()) - len(set(line.split())) - 1)
-cat examples/04 | head -1 | tr -d '|' | awk '{ for(i=3;i<=NF;i++) print cs[$i]+=1; } END { for(c in cs) print c; for(c in cs) if (cs[c]>1) {s+=1; print cs[c], s;}; print (2 ^ (s - 1)) }'
+cat examples/04 | tr -d '|' | awk '
+  {
+    p=0;
+    for(i=3;i<=NF;i++) t[$i]+=1
+    for(k in t) if(t[k]>1) p+=1
+    if(p>0) s+=2^(p-1)
+    delete t
+  }
+  END { print s }
+'
