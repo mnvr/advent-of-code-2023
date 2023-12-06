ebc78ec270a2758f2fc2bcbbedde188211297898
Manav <manav@mrmr.io>
2023-12-06 15:06:00 +0530

    The problem is already topologically sorted!
    I realized that we don't have to memoize, we can just build the table
    in the reverse order. This is for part 2.
    
    The input might not have a trailing newline, which causes wc -l to
    print one less count, so use awk to count the lines instead.

--- 04.az.sh
+++ 04.az.sh
@@ -2,13 +2,21 @@
 
 test -z "$1" && echo "usage: $0 <path-to-input>" && exit 1
 
-cat "$1" | tr -d '|' | awk '
+nl=`awk 'END { print NR }' "$1"`
+echo $nl
+
+cat "$1" | tr -d '|' | nl | sort -nr | cut -f2- | awk -vnl=$nl '
   {
-    p=0;
+    p=0; s=0;
     for(i=3;i<=NF;i++) t[$i]+=1
     for(k in t) if(t[k]>1) p+=1
     if(p>0) s+=2^(p-1)
     delete t
+    count=1
+    # for(i=p;)
+    memo[NR]=count
+    ss += s
+    print NR, nl-NR, p, s, ss
   }
-  END { print s }
+  END { print ss; for(i=1;i<=nl;i++) print i, memo[i]; }
 '
