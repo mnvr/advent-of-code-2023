d60ec46e887602c6ee8a4b2191bedcdc9ad4f7d3
Manav <manav@mrmr.io>
2023-12-06 07:28:05 +0530

    Simplify more

--- 05.hs
+++ 05.hs
@@ -1,7 +1,5 @@
 import Text.Parsec
-import Text.Parsec.String (Parser)
 import Control.Monad (void)
-import Data.Char (isSpace)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . ((,) <$> p1 <*> p2) . parseAlmanac
@@ -10,7 +8,7 @@ data Almanac = Almanac { seeds :: [Int], maps :: [RangesMap] } deriving Show
 type RangesMap = [RangeMap]
 data RangeMap = RangeMap { destinationRange :: Int, sourceRange :: Int, rangeLength :: Int } deriving Show
 
--- parseAlmanac :: String -> Input
+parseAlmanac :: String -> Almanac
 parseAlmanac s = case parse almanac "" s of
     Left err -> error (show err)
     Right v -> v
@@ -18,23 +16,14 @@ parseAlmanac s = case parse almanac "" s of
      sp = char ' '
      num = read <$> many1 digit
      nums = num `sepBy` sp
+     seeds = string "seeds: " *> nums <* count 2 newline
      mapHeader = many1 (letter <|> char '-' <|> sp) >> char ':'
      endOfLineOrFile = void endOfLine <|> eof
      range = (,,) <$> (num <* sp) <*> (num <* sp) <*> num
      ranges = range `endBy` endOfLineOrFile
-     map = do
-        mapHeader
-        newline
-        ranges
-     maps = do
-        many1 (map >>= \m -> endOfLineOrFile >> pure m)
-     almanac = do
-        string "seeds: "
-        seeds <- nums
-        newline
-        newline
-        m <- maps
-        pure $ Almanac seeds (fmap convRangesMap m)
+     map = mapHeader *> newline *> ranges
+     maps = map `endBy` endOfLineOrFile
+     almanac = Almanac <$> seeds <*> (fmap convRangesMap <$> maps)
      convRangesMap :: [(Int,Int,Int)] -> RangesMap
      convRangesMap = fmap convRangeMap
      convRangeMap :: (Int,Int,Int) -> RangeMap
