0af288059c54bc6bf86be4dc556553dd94eef2f7
Manav <manav@mrmr.io>
2023-12-07 17:25:18 +0530

    Tinker

--- 07.hs
+++ 07.hs
@@ -1,6 +1,7 @@
 import Data.List (sortBy, nub)
 import Data.Bifunctor (first)
 import Data.Monoid -- (Sum)
+import Control.Arrow ((&&&))
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parseHands
@@ -10,7 +11,8 @@ type Hand = ((String, HandType), Int)
 data HandType = Five | Four | Full | Three | Two | One | High deriving Show
 
 cardCounts :: String -> [(Char, Int)]
-cardCounts = foldl (\as c -> fmap (+1) ((lookup c as) <> Just (Sum 1)) : as) []
+cardCounts = foldl (\as c -> incr as c : as) [] where
+  incr as c = case lookup c as of Nothing -> (c, 1); Just i -> (c, i + 1)
 
 mkHandType :: String -> HandType
 mkHandType _ = Five
@@ -25,7 +27,9 @@ parseHands = map (first (\x -> (x, mkHandType x)) . fmap read . span (/= ' ')) .
 compareHands :: Hand ->Hand -> Ordering
 compareHands (d, _) (d', _) = GT
 
-p1 :: [Hand] -> [Hand]
+-- p1 :: [Hand] -> []
 -- p1 = sortBy compareCards
 -- p1 xs = map (first . first nub) xs
-p1 xs = xs
+-- p1 :: [Hand] -> [(String, [(Char, Int)])]
+-- p1 :: [((a, String), b)] -> [(((a, String), b), [(Char, Int)])]
+p1 = map (id &&& cardCounts . fst . fst) -- (cardCounts . fst)) -- (\x -> (x, cardCounts (snd $ fst x)))
