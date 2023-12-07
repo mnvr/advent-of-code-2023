ece9b07360fad2003ac6a4d43013ccb3ab23c4ba
Manav <manav@mrmr.io>
2023-12-07 17:38:01 +0530

    Classify

--- 07.hs
+++ 07.hs
@@ -14,14 +14,16 @@ cardCounts :: String -> [(Char, Int)]
 cardCounts = foldl (\as c -> incr as c : as) [] where
   incr as c = case lookup c as of Nothing -> (c, 1); Just i -> (c, i + 1)
 
+maxCardCount :: String -> Int
+maxCardCount = maximum . map snd . cardCounts
+
 mkHandType :: String -> HandType
-mkHandType s = case length u of
+mkHandType s = case (length . nub) s of
       1 -> Five
-      2 -> Four -- or Full
-      3 -> Two -- or Three
+      2 -> if maxCardCount s == 4 then Four else Full
+      3 -> if maxCardCount s == 3 then Three else Two
       4 -> One
       5 -> High
-    where u = nub s
 
 parseHands :: String -> [Hand]
 parseHands = map (first (\x -> (x, mkHandType x)) . fmap read . span (/= ' ')) . lines
