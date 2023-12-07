4465238ef69ec322e54676270802fe9653699aca
Manav <manav@mrmr.io>
2023-12-07 13:47:47 +0530

    Point free

--- 04.hs
+++ 04.hs
@@ -7,14 +7,14 @@ main = interact $ (++ "\n") . show . ((,) <$> p1 <*> p2) . parseCards
 type Card = ([Int], [Int])
 
 parseCards :: String -> [Card]
-parseCards s = map (bimap nums nums . span (/= "|") . tail . words) (lines s)
+parseCards = map (bimap nums nums . span (/= "|") . tail . words) . lines
   where nums = map read . tail
 
 p1 :: [Card] -> Int
 p1 = sum . map points
 
 matches :: Card -> Int
-matches (xs, ys) = length $ intersect xs ys
+matches = length . uncurry intersect
 
 points :: Card -> Int
 points card = case matches card of 0 -> 0; n -> 2 ^ (n - 1)
@@ -22,5 +22,5 @@ points card = case matches card of 0 -> 0; n -> 2 ^ (n - 1)
 p2 :: [Card] -> Int
 p2 = sum . map (+1) . foldr f [] where
   f card wins = w : wins where
-    p = matches card
-    w = p + sum (take p wins)
+    m = matches card
+    w = m + sum (take m wins)
