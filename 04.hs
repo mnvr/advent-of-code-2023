abd6f138b14f5530b1af135a14888db9853c54e2
Manav <manav@mrmr.io>
2023-12-07 13:35:54 +0530

    Swap

--- 04.hs
+++ 04.hs
@@ -1,67 +1,33 @@
-{-# LANGUAGE LambdaCase #-}
-
-import Control.Monad.State
-import Data.Map qualified as M
-import Text.Parsec
-
--- Use Parsec to parse, and the State monad to memoize
---
--- I also wrote a blog post about how to use the State monad to memoize. This
--- post can also serve as an tutorial introduction to the State monad:
--- https://mrmr.io/memoization-in-haskell
+import Data.Bifunctor (bimap)
+import Text.Read (readMaybe)
+import Data.Char (isSpace)
+import Data.Maybe (catMaybes)
+import Data.List (intersect)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . ((,) <$> p1 <*> p2) . parseCards
 
-data Card = Card { winning :: [Int], have :: [Int] } deriving (Show)
+type Card = ([Int], [Int])
 
 parseCards :: String -> [Card]
-parseCards s = case parse cards "" s of
-    Left err -> error (show err)
-    Right v -> v
-  where
-    cards = many1 card
-    card = Card <$> (prelude *> nums <* char '|') <*> nums
-    prelude = string "Card" *> spaces *> num *> char ':'
-    num = read <$> many1 digit
-    nums = many1 (between spaces spaces num)
+parseCards s = map (bimap nums nums . span (/= '|')) (lines s)
 
-matches :: Card -> Int
-matches Card { winning, have } = length $ filter (`elem` have) winning
-
-points :: Card -> Int
-points = score . matches where score 0 = 0
-                               score n = 2 ^ (n - 1)
+nums :: String -> [Int]
+nums = catMaybes . nums' where
+  nums' [] = []
+  nums' s = let (a, b) = break isSpace s in readMaybe a : nums' (dropWhile isSpace b)
 
 p1 :: [Card] -> Int
 p1 = sum . map points
 
-wins :: [Card] -> Int -> [Int]
-wins cards i = case matches (cards !! i) of
-    0 -> []
-    n -> [(i+1)..(i+n)]
+matches :: Card -> Int
+matches (xs, ys) = length $ intersect xs ys
 
-memo :: (MonadState (M.Map k b) m, Ord k) => k -> m b -> m b
-memo i compute = gets (M.lookup i) >>= \case
-  Just v -> pure v
-  Nothing -> compute >>= \r -> modify (M.insert i r) >> pure r
-
-winsRec :: MonadState (M.Map Int [Int]) m => [Card] -> Int -> m [Int]
-winsRec cards i = memo i (_winsRec cards i)
-
--- Note - for recursive calls, this "actual" version must call the memoized, non
--- underscore version for the memoization to kick in.
-_winsRec :: MonadState (M.Map Int [Int]) f => [Card] -> Int -> f [Int]
-_winsRec cards i = case wins cards i of
-  [] -> pure []
-  ys -> do
-    r' <- let f prev y = (: prev) <$> winsRec cards y in foldM f [] ys
-    pure $ concat (ys : r')
-
-allWins :: MonadState (M.Map Int [Int]) m => [Card] -> m [Int]
-allWins cards = foldM f [] [0..length cards - 1]
-  where f w i = winsRec cards i >>= \extra ->
-         pure ((1 + length extra) : w)
+points :: Card -> Int
+points card = case matches card of 0 -> 0; n -> 2 ^ (n - 1)
 
 p2 :: [Card] -> Int
-p2 cards = sum $ evalState (allWins cards) M.empty
+p2 = sum . map (+1) . foldr f [] where
+  f card wins = w : wins where
+    p = matches card
+    w = p + sum (take p wins)
---
+++ 04.memo-map.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE LambdaCase #-}
+
+import Control.Monad.State
+import Data.Map qualified as M
+import Text.Parsec
+
+-- Use Parsec to parse, and the State monad to memoize
+--
+-- I also wrote a blog post about how to use the State monad to memoize. This
+-- post can also serve as an tutorial introduction to the State monad:
+-- https://mrmr.io/memoization-in-haskell
+
+main :: IO ()
+main = interact $ (++ "\n") . show . ((,) <$> p1 <*> p2) . parseCards
+
+data Card = Card { winning :: [Int], have :: [Int] } deriving (Show)
+
+parseCards :: String -> [Card]
+parseCards s = case parse cards "" s of
+    Left err -> error (show err)
+    Right v -> v
+  where
+    cards = many1 card
+    card = Card <$> (prelude *> nums <* char '|') <*> nums
+    prelude = string "Card" *> spaces *> num *> char ':'
+    num = read <$> many1 digit
+    nums = many1 (between spaces spaces num)
+
+matches :: Card -> Int
+matches Card { winning, have } = length $ filter (`elem` have) winning
+
+points :: Card -> Int
+points = score . matches where score 0 = 0
+                               score n = 2 ^ (n - 1)
+
+p1 :: [Card] -> Int
+p1 = sum . map points
+
+wins :: [Card] -> Int -> [Int]
+wins cards i = case matches (cards !! i) of
+    0 -> []
+    n -> [(i+1)..(i+n)]
+
+memo :: (MonadState (M.Map k b) m, Ord k) => k -> m b -> m b
+memo i compute = gets (M.lookup i) >>= \case
+  Just v -> pure v
+  Nothing -> compute >>= \r -> modify (M.insert i r) >> pure r
+
+winsRec :: MonadState (M.Map Int [Int]) m => [Card] -> Int -> m [Int]
+winsRec cards i = memo i (_winsRec cards i)
+
+-- Note - for recursive calls, this "actual" version must call the memoized, non
+-- underscore version for the memoization to kick in.
+_winsRec :: MonadState (M.Map Int [Int]) f => [Card] -> Int -> f [Int]
+_winsRec cards i = case wins cards i of
+  [] -> pure []
+  ys -> do
+    r' <- let f prev y = (: prev) <$> winsRec cards y in foldM f [] ys
+    pure $ concat (ys : r')
+
+allWins :: MonadState (M.Map Int [Int]) m => [Card] -> m [Int]
+allWins cards = foldM f [] [0..length cards - 1]
+  where f w i = winsRec cards i >>= \extra ->
+         pure ((1 + length extra) : w)
+
+p2 :: [Card] -> Int
+p2 cards = sum $ evalState (allWins cards) M.empty
