ee014e504e2ce06fd6319ddd486521bfe729ae21
Manav <manav@mrmr.io>
2023-12-07 16:52:15 +0530

    Scaffold sort

--- 07.hs
+++ 07.hs
@@ -1,7 +1,14 @@
+import Data.List (sortBy)
 main :: IO ()
-main = interact $ (++ "\n") . show . parseCards
+main = interact $ (++ "\n") . show . p1 . parseCards
 
 type Card = (String, Int)
 
 parseCards :: String -> [Card]
 parseCards = map (fmap read . span (/= ' ')) . lines
+
+compareCards :: Card -> Card -> Ordering
+compareCards (d, _) (d', _) = GT
+
+-- p1 :: [Card] ->
+p1 = sortBy compareCards
