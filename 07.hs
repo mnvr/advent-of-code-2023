49f8768e8ff6effad362770be1b5b50112c2254e
Manav <manav@mrmr.io>
2023-12-07 22:34:57 +0530

    p2

--- 07.hs
+++ 07.hs
@@ -1,9 +1,11 @@
-import Data.List (sortBy, nub, elemIndex, sortOn, delete)
+import Data.List (sortOn, nub, elemIndex, delete, minimumBy)
 import Data.Maybe (fromJust)
 import Data.Ord (Down(Down))
+import Data.Function (on)
+import Control.Arrow ((&&&))
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parseHands
+main = interact $ (++ "\n") . show . (p1 &&& p2) . parseHands
 
 type Hand = (String, Int)
 
@@ -16,8 +18,8 @@ data HandAttr = HandAttr { htype :: HandType, hvalue :: Int }
 data HandType = Five | Four | Full | Three | Two | One | High
     deriving (Show, Eq, Ord)
 
-handType :: String -> HandType
-handType s = case (length . nub) s of
+handType :: Hand -> HandType
+handType (s, _) = case (length . nub) s of
       1 -> Five
       2 -> if maxCardCount s == 4 then Four else Full
       3 -> if maxCardCount s == 3 then Three else Two
@@ -31,19 +33,31 @@ cardCounts :: String -> [(Char, Int)]
 cardCounts = foldl (\as c -> incr as c : as) [] where
   incr as c = case lookup c as of Nothing -> (c, 1); Just i -> (c, i + 1)
 
-handValue :: [Char] -> String -> Int
-handValue labelStrength =
-    foldl1 (\v i -> v * 100 + i) . map (fromJust . (`elemIndex` labelStrength))
+handValue :: [Char] -> Hand -> Int
+handValue labelStrength (s, _) =
+    foldl1 (\v i -> v * 100 + i) . map (fromJust . (`elemIndex` labelStrength)) $ s
 
-labelStrength :: [Char]
-labelStrength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+cards :: [Char]
+cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
 
 p1 :: [Hand] -> Int
-p1 = winnings (\(s, _) -> HandAttr (handType s) (handValue labelStrength s))
+p1 = winnings (\h -> HandAttr (handType h) (handValue cards h))
 
 winnings :: (Hand -> HandAttr) -> [Hand] -> Int
 winnings mkAttr = sum . zipWith (*) [1..] . map snd . sortOn (Down . mkAttr)
 
 p2 :: [Hand] -> Int
-p2 = winnings (\(s, _) -> HandAttr (handType s) (handValue labelStrength s))
-  where labelStrengthJ = delete 'J' labelStrength ++ ['J']
+p2 = winnings (\h -> HandAttr (bestHandType h) (handValue cardsJ h))
+  where cardsJ = delete 'J' cards ++ ['J']
+        bestHandType = handType . highestRanked . jokerVariants
+
+highestRanked :: [Hand] -> Hand
+highestRanked = minimumBy (compare `on` handType)
+
+jokerVariants :: Hand -> [Hand]
+jokerVariants h@(s, v) = if hasJoker h then variations else [h]
+  where variations = [(replaceJ c, v) | c <- cards]
+        replaceJ r = map (\c -> if c == 'J' then r else c) s
+
+hasJoker :: Hand -> Bool
+hasJoker (s, _) = 'J' `elem` s
