66a3389cafc5452d14d0ddb78f81eb4f6bd8b8c0
Manav <manav@mrmr.io>
2023-12-07 18:07:28 +0530

    Sort

--- 07.hs
+++ 07.hs
@@ -1,18 +1,19 @@
-import Data.List (sortBy, nub)
+import Data.List (sortBy, nub, elemIndex, find)
 import Data.Bifunctor (first)
 import Data.Monoid -- (Sum)
 import Control.Arrow ((&&&))
+import Data.Maybe (fromMaybe, fromJust)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parseHands
 
 type Hand = ((String, HandType), Int)
 
-data HandType = Five | Four | Full | Three | Two | One | High deriving Show
+data HandType = Five | Four | Full | Three | Two | One | High deriving (Show, Eq, Ord)
 
 cardCounts :: String -> [(Char, Int)]
 cardCounts = foldl (\as c -> incr as c : as) [] where
-  incr as c = case lookup c as of Nothing -> (c, 1); Just i -> (c, i + 1)
+  incr as c = case lookup c as of Nothing -> ('c', 1); Just i -> ('c', i + 1)
 
 maxCardCount :: String -> Int
 maxCardCount = maximum . map snd . cardCounts
@@ -28,12 +29,13 @@ mkHandType s = case (length . nub) s of
 parseHands :: String -> [Hand]
 parseHands = map (first (\x -> (x, mkHandType x)) . fmap read . span (/= ' ')) . lines
 
-compareHands :: Hand ->Hand -> Ordering
-compareHands (d, _) (d', _) = GT
+compareHands :: Hand -> Hand -> Ordering
+compareHands ((s,t), _) ((s',t'), _) = let ot = compare t t' in
+    if ot /= EQ then ot else fromMaybe EQ $ find (/= EQ) (zipWith compareLabel s s')
+    where compareLabel c c' = compare (labelIndex c) (labelIndex c')
+          labelIndex c = fromJust (elemIndex c labelStrength)
+
+labelStrength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
 
 -- p1 :: [Hand] -> []
--- p1 = sortBy compareCards
--- p1 xs = map (first . first nub) xs
--- p1 :: [Hand] -> [(String, [(Char, Int)])]
--- p1 :: [((a, String), b)] -> [(((a, String), b), [(Char, Int)])]
-p1 = map (id &&& cardCounts . fst . fst) -- (cardCounts . fst)) -- (\x -> (x, cardCounts (snd $ fst x)))
+p1 = sortBy compareHands
