f640cc862b1c0071658f48102afe43cc5a2e03cc
Manav <manav@mrmr.io>
2023-12-07 17:02:03 +0530

    Create type

--- 07.hs
+++ 07.hs
@@ -1,14 +1,22 @@
-import Data.List (sortBy)
+import Data.List (sortBy, nub)
+import Data.Bifunctor (first)
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parseCards
 
-type Card = (String, Int)
+type Card = ((String, CardType), Int)
+
+data CardType = Five | Four | Full | Three | Two | One | High deriving Show
+
+mkCardType :: String -> CardType
+mkCardType s = High
 
 parseCards :: String -> [Card]
-parseCards = map (fmap read . span (/= ' ')) . lines
+parseCards = map (first (\x -> (x, mkCardType x)) . fmap read . span (/= ' ')) . lines
 
 compareCards :: Card -> Card -> Ordering
 compareCards (d, _) (d', _) = GT
 
--- p1 :: [Card] ->
-p1 = sortBy compareCards
+p1 :: [Card] -> [Card]
+-- p1 = sortBy compareCards
+-- p1 xs = map (first . first nub) xs
+p1 xs = xs
