f8bde4a47dd68baa299d285be7a37777b4d41c50
Manav <manav@mrmr.io>
2023-12-07 18:37:52 +0530

    Cleanup

--- 07.hs
+++ 07.hs
@@ -1,7 +1,5 @@
-import Data.List (sortBy, nub, elemIndex, find)
+import Data.List (sortBy, nub, elemIndex)
 import Data.Bifunctor (first)
-import Control.Arrow ((&&&))
-import Data.Maybe (fromMaybe, fromJust)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parseHands
@@ -10,12 +8,8 @@ type Hand = ((String, HandType), Int)
 
 data HandType = Five | Four | Full | Three | Two | One | High deriving (Show, Eq, Ord)
 
-cardCounts :: String -> [(Char, Int)]
-cardCounts = foldl (\as c -> incr as c : as) [] where
-  incr as c = case lookup c as of Nothing -> (c, 1); Just i -> (c, i + 1)
-
-maxCardCount :: String -> Int
-maxCardCount = maximum . map snd . cardCounts
+parseHands :: String -> [Hand]
+parseHands = map (first (\x -> (x, mkHandType x)) . fmap read . span (/= ' ')) . lines
 
 mkHandType :: String -> HandType
 mkHandType s = case (length . nub) s of
@@ -25,22 +19,26 @@ mkHandType s = case (length . nub) s of
       4 -> One
       5 -> High
 
-parseHands :: String -> [Hand]
-parseHands = map (first (\x -> (x, mkHandType x)) . fmap read . span (/= ' ')) . lines
+cardCounts :: String -> [(Char, Int)]
+cardCounts = foldl (\as c -> incr as c : as) [] where
+  incr as c = case lookup c as of Nothing -> (c, 1); Just i -> (c, i + 1)
+
+maxCardCount :: String -> Int
+maxCardCount = maximum . map snd . cardCounts
 
 compareHands :: Hand -> Hand -> Ordering
-compareHands ((s,t), _) ((s',t'), _) = let ot = compare t t' in
-    if ot /= EQ then ot else fromMaybe EQ $ find (/= EQ) (zipWith compareLabel s s')
-    where compareLabel c c' = compare (labelIndex c) (labelIndex c')
-          labelIndex c = fromJust (elemIndex c labelStrength)
+compareHands ((s,t), _) ((s',t'), _) =
+    let o = compare t t' in if o == EQ then compareCards s s' else o
+
+compareCards :: [Char] -> [Char] -> Ordering
+compareCards s s' = compare (labels s) (labels s')
+  where labels = map (`elemIndex` labelStrength)
 
 labelStrength :: [Char]
 labelStrength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
 
-enumerate :: [a] -> [(Int, a)]
-enumerate = zip [0..]
-
 p1 :: [Hand] -> Int
-p1 = sum . map score . enumerate . sorted
-  where sorted  = sortBy (flip compareHands)
-        score (i, (_, j)) = (i + 1) * j
+p1 = winnings compareHands
+
+winnings :: (Hand -> Hand -> Ordering) -> [Hand] -> Int
+winnings cmp = sum . zipWith (*) [1..] . map snd . sortBy (flip cmp)
