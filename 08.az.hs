c27e826a32ad12102c7b8c8974d730057dd0763d
Manav <manav@mrmr.io>
2023-12-08 16:52:35 +0530

    Golf more

--- 08.az.hs
+++ 08.az.hs
@@ -2,19 +2,19 @@ import Data.Bifunctor
 import Control.Arrow ((&&&))
 import Data.Map qualified as M
 
-main=interact$(++ "\n").show.(स &&& ग).प
+main=interact$(++"\n").show.(स&&&ग).प
 
-प=bimap head (M.fromList.map ऋ).splitAt 2. lines
-ऋ=second (द.drop 3).त
-द=second (fst.त.drop 2).त.tail
+प=bimap head(M.fromList.map ऋ).splitAt 2.lines
+ऋ=second(द.drop 3).त
+द=second(fst.त.drop 2).त.tail
 त=splitAt 3
-स=म "AAA"
+स=म"AAA"
 
-म न (ष, ऋ)=length$य (cycle ष) न ऋ
-य _ [_,_,'Z'] _=[]
-य (व:ष) न ऋ=():य ष (झ व$ऋ M.!न) ऋ
+म न(ष,ऋ)=length$य(cycle ष)न ऋ
+य _[_,_,'Z']_=[]
+य(व:ष)न ऋ=():य ष(झ व$ऋ M.!न)ऋ
 झ 'L'=fst
 झ _=snd
 
-ग input@(_, ऋ)=foldl1 lcm$map (`म` input)$ए ऋ
-ए ऋ=filter ((== 'A').last)$M.keys ऋ
+ग भ@(_,ऋ)=foldl1 lcm$map(`म`भ)$ए ऋ
+ए ऋ=filter((=='A').last)$M.keys ऋ
