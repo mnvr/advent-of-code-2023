afd0dc800adb9d634464db9ace848bc591941e97
Manav <manav@mrmr.io>
2023-12-08 16:46:26 +0530

    Golf

--- 08.az.hs
+++ 08.az.hs
@@ -2,17 +2,19 @@ import Data.Bifunctor
 import Control.Arrow ((&&&))
 import Data.Map qualified as M
 
-main = interact $ (++ "\n") . show . (स &&& ग) . प
+main=interact$(++ "\n").show.(स &&& ग).प
 
-प = bimap head (M.fromList . map ऋ) . splitAt 2. lines
-ऋ = second (द . drop 3) . त
-द = second (fst . त . drop 2) . त . tail
-त = splitAt 3
-स = म "AAA"
+प=bimap head (M.fromList.map ऋ).splitAt 2. lines
+ऋ=second (द.drop 3).त
+द=second (fst.त.drop 2).त.tail
+त=splitAt 3
+स=म "AAA"
 
-म न (ष, ऋ) = length $ य (cycle ष) न ऋ
-य _ [_, _, 'Z'] _ = []
-य (i:ष) न ऋ = () : य ष ((if i == 'L' then fst else snd) $ ऋ M.! न) ऋ
+म न (ष, ऋ)=length$य (cycle ष) न ऋ
+य _ [_,_,'Z'] _=[]
+य (व:ष) न ऋ=():य ष (झ व$ऋ M.!न) ऋ
+झ 'L'=fst
+झ _=snd
 
-ग input@(_, ऋ) = foldl1 lcm $ map (`म` input) $ ए ऋ
-ए ऋ = filter ((== 'A') . last) $ M.keys ऋ
+ग input@(_, ऋ)=foldl1 lcm$map (`म` input)$ए ऋ
+ए ऋ=filter ((== 'A').last)$M.keys ऋ
