95f428a0b2c05fa25312f73f4e7cb9e44dc0f6af
Manav <manav@mrmr.io>
2023-12-08 18:59:02 +0530

    Reduce two more, 481 chars now

--- 08.az.hs
+++ 08.az.hs
@@ -1,6 +1,6 @@
-import Data.Bifunctor;
-import Data.Map qualified as M;प=bimap head(M.fromList.map ऋ).क 2.lines
-main=interact$(++"\n").show.((,)<$>स<*>ग).प;ए ऋ=filter((=='A').last)$M.keys ऋ
-ऋ=इ(द.drop 3).त;य _[_,_,'Z']_=[];य(व:ष)न ऋ=():य ष(झ व$ऋ M.!न)ऋ;क=splitAt
-म न(ष,ऋ)=length$य(cycle ष)न ऋ;द=इ(fst.त.drop 2).त.tail;स=म"AAA";त=क 3
-ग भ@(_,ऋ)=foldl1 lcm$map(`म`भ)$ए ऋ;झ 'L'=fst;झ _=snd;इ=fmap
+import Data.Map qualified as M;import Data.Bifunctor;स=म"AAA";क=splitAt
+main=interact$(++"\n").show.((,)<$>स<*>ग).प;झ 'L'=fst;झ _=snd;इ=fmap
+ग भ@(_,ऋ)=foldl1 lcm$map(`म`भ)$ए ऋ;ए ऋ=filter((=='A').last)$M.keys ऋ
+य _[_,_,'Z']_=[];य(व:ष)न ऋ=():य ष(झ व$ऋ M.!न)ऋ;त=क 3;ऋ=इ(द.drop 3).त
+प=bimap head(M.fromList.map ऋ).क 2.lines;द=इ(fst.त.drop 2).त.tail
+म न(ष,ऋ)=length$य(cycle ष)न ऋ
