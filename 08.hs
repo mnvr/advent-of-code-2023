de96e487f870ca6d447f3d292c35f5bf23f1895d
Manav <manav@mrmr.io>
2023-12-08 16:21:47 +0530

    Use map instead
    It halves the runtime

--- 08.hs
+++ 08.hs
@@ -1,16 +1,17 @@
 import Data.Bifunctor (bimap, second)
 import Data.Maybe (fromJust)
 import Control.Arrow ((&&&))
+import Data.Map qualified as M
 
 main :: IO ()
 main = interact $ (++ "\n") . show . (p1 &&& p2) . parse
 
 type Instructions = String
 type Node = String
-type Network = [(Node, (Node, Node))]
+type Network = M.Map Node (Node, Node)
 
 parse :: String -> (Instructions, Network)
-parse = bimap head (map network) . splitAt 2. lines
+parse = bimap head (M.fromList . map network) . splitAt 2. lines
   where network = second (pair . drop 3) . label
         pair = second (fst . label . drop 2) . label . tail
         label = splitAt 3
@@ -22,11 +23,11 @@ p1 = pathLength "AAA"
 pathLength :: Node -> (Instructions, Network) -> Int
 pathLength node (is, network) = length $ path (cycle is) node
   where path _ [_, _, 'Z'] = []
-        path (i:is) node = () : path is (move i $ fromJust $ lookup node network)
+        path (i:is) node = () : path is (move i $ fromJust $ M.lookup node network)
 
 move :: Char -> (Node, Node) -> Node
 move i = if i == 'L' then fst else snd
 
 p2 :: (Instructions, Network) -> Int
 p2 input@(_, network) = foldl1 lcm $ map (`pathLength` input) startNodes
-  where startNodes = filter ((== 'A') . last) $ map fst network
+  where startNodes = filter ((== 'A') . last) $ M.keys network
