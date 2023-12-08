58e55a686184f0935a82322097442daaec0c9e37
Manav <manav@mrmr.io>
2023-12-08 16:45:01 +0530

    Flatten

--- 08.az.hs
+++ 08.az.hs
@@ -5,15 +5,14 @@ import Data.Map qualified as M
 main = interact $ (++ "\n") . show . (स &&& ग) . प
 
 प = bimap head (M.fromList . map ऋ) . splitAt 2. lines
-  where ऋ = second (द . drop 3) . त
-        द = second (fst . त . drop 2) . त . tail
-        त = splitAt 3
-
+ऋ = second (द . drop 3) . त
+द = second (fst . त . drop 2) . त . tail
+त = splitAt 3
 स = म "AAA"
 
-म न (ष, ऋ) = length $ य (cycle ष) न
-  where य _ [_, _, 'Z'] = []
-        य (i:ष) न = () : य ष ((if i == 'L' then fst else snd) $ ऋ M.! न)
+म न (ष, ऋ) = length $ य (cycle ष) न ऋ
+य _ [_, _, 'Z'] _ = []
+य (i:ष) न ऋ = () : य ष ((if i == 'L' then fst else snd) $ ऋ M.! न) ऋ
 
-ग input@(_, ऋ) = foldl1 lcm $ map (`म` input) ए
-  where ए = filter ((== 'A') . last) $ M.keys ऋ
+ग input@(_, ऋ) = foldl1 lcm $ map (`म` input) $ ए ऋ
+ए ऋ = filter ((== 'A') . last) $ M.keys ऋ
