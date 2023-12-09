5cc1d90542877b0264431453b593dfbe5c22f44b
Manav <manav@mrmr.io>
2023-12-09 11:53:12 +0530

    foldl

--- 09.hs
+++ 09.hs
@@ -4,19 +4,27 @@ import Data.Char (isSpace)
 import Data.Bifunctor (bimap)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parse
+main = interact $ (++ "\n") . show . p2 . parse
 
 nums :: String -> [Int]
 nums [] = []
 nums s = uncurry (:) <$> bimap read nums $ break isSpace (dropWhile isSpace s)
 
 parse :: String -> [[Int]]
-parse = map nums . lines
+parse = map nums . drop 2 . lines
 
 -- p1 :: [Int] -> [[Int]]
 p1 xss = sum $ map p1' xss
 
 p1' xs = last . head $ fst $ foldr (\ds (yss, d) -> ((ds ++ [last ds + d]) : yss, last ds + d)) ([], 0) $ dxs xs
+
+-- p2 :: [[Int]] -> Int
+p2 xss = p2' (head xss)--map p2' xss
+
+-- p2' :: [Int] -> Int
+-- p2' :: [Int] -> [[Int]]
+p2' xs = foldl (\(d, yss) ds -> (d, yss ++ [ds])) (0,[]) $ dxs xs
+--foldl (\(d, yss) ds -> (d + head ds, (d + head ds : yss))) (0, []) $ dxs xs
 
 dxs xs = if az xs then [xs] else let d = dx xs in xs : dxs d
   where az = all (==0)
