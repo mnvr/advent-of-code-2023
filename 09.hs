d7736203705cf1178b2c7c0824f09d1da2b81318
Manav <manav@mrmr.io>
2023-12-09 12:02:03 +0530

    Cleanup a bit

--- 09.hs
+++ 09.hs
@@ -4,30 +4,32 @@ import Data.Char (isSpace)
 import Data.Bifunctor (bimap)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p2 . parse
+main = interact $ (++ "\n") . show . p2a . parse
+
+parse :: String -> [[Int]]
+parse = map nums . lines
 
 nums :: String -> [Int]
 nums [] = []
 nums s = uncurry (:) <$> bimap read nums $ break isSpace (dropWhile isSpace s)
 
-parse :: String -> [[Int]]
-parse = map nums . lines
+p1 :: [[Int]] -> Int
+p1 = sum . map (foldr (\ds d -> last ds + d) 0 . dxs)
 
--- p1 :: [Int] -> [[Int]]
-p1 xss = sum $ map p1' xss
-
-p1' xs = last . head $ fst $ foldr (\ds (yss, d) -> ((ds ++ [last ds + d]) : yss, last ds + d)) ([], 0) $ dxs xs
+p2a :: [[Int]] -> Int
+p2a = map (foldl (\d ds -> head ds - d) 0 . reverse . dxs)
 
 -- p2 :: [[Int]] -> Int
-p2 xss = sum $ map p2' xss
+-- p2 xss = sum $ map p2' xss
+p2 xss = map p2' xss
 
 -- p2' :: [Int] -> Int
 -- p2' :: [Int] -> [[Int]]
 p2' xs = fst $ foldl (\(d, yss) ds -> (head ds - d, yss ++ [ds])) (0,[]) $ dxs xs
 --foldl (\(d, yss) ds -> (d + head ds, (d + head ds : yss))) (0, []) $ dxs xs
 
-dxs xs = if az xs then [xs] else let d = dx xs in xs : dxs d
-  where az = all (==0)
+dxs :: [Int] -> [[Int]]
+dxs xs = if all (==0) xs then [xs] else xs : dxs (dx xs)
 
 dx :: [Int] -> [Int]
 dx xs = zipWith (-) (drop 1 $ xs) xs
