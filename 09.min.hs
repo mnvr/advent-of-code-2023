7a485b4b928c7330b59ce37b40c023c37a179a16
Manav <manav@mrmr.io>
2023-12-09 13:56:38 +0530

    164 chars

--- 09.min.hs
+++ 09.min.hs
@@ -1,3 +1,3 @@
 main=interact$(++"\n").show.((,)<$>a<*>a.map reverse).map(map read.words).lines
-w z=if all(==0)z then[z]else z:w(zipWith(-)(drop 1$z)z)
-a=sum.map(foldr(\u d->last u+d)0.w)
+a=sum.map(sum.map last.w);w=takeWhile(any(/=0)).iterate d
+d z=zipWith(-)(drop 1$z)z
