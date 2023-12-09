ea8228d31170ba6cf457465d16096058709c1328
Manav <manav@mrmr.io>
2023-12-09 12:58:30 +0530

    172 chars

--- 09.min.hs
+++ 09.min.hs
@@ -1,5 +1,3 @@
-main=interact$(++"\n").show.((,)<$>a<*>b).parse
-parse=map(map read.words).lines
-a=sum.map(foldr(\u d->last u+d)0.w)
-b=a.map reverse
+main=interact$(++"\n").show.((,)<$>a<*>a.map reverse).map(map read.words).lines
 w z=if all(==0)z then[z]else z:w(zipWith(-)(drop 1$z)z)
+a=sum.map(foldr(\u d->last u+d)0.w)
