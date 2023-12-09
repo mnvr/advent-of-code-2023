49d9409cc1127b099168e494ee7fa201feb4b1e2
Manav <manav@mrmr.io>
2023-12-09 11:40:14 +0530

    Extract lasts

--- 09.hs
+++ 09.hs
@@ -10,11 +10,13 @@ nums :: String -> [Int]
 nums [] = []
 nums s = uncurry (:) <$> bimap read nums $ break isSpace (dropWhile isSpace s)
 
-parse :: String -> [Int]
+-- parse :: String -> [Int]
 parse = nums . head . lines
 
-p1 :: [Int] -> [[Int]]
-p1 xs = fst $ foldr (\ds (yss, d) -> ((ds ++ [last ds + d]) : yss, last ds + d)) ([], 0) $ dxs xs
+-- p1 :: [Int] -> [[Int]]
+p1 xss = sum $ [p1' xss]
+
+p1' xs = last . head $ fst $ foldr (\ds (yss, d) -> ((ds ++ [last ds + d]) : yss, last ds + d)) ([], 0) $ dxs xs
 
 dxs xs = if az xs then [xs] else let d = dx xs in xs : dxs d
   where az = all (==0)
