27d1bb2a79f4b9508483f1df6cd06ca9c7dbf4f3
Manav <manav@mrmr.io>
2023-12-10 11:58:22 +0530

    Ensure (and fix) start

--- 10.hs
+++ 10.hs
@@ -1,17 +1,22 @@
 import Data.Map qualified as M
+import Data.Maybe (catMaybes)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parse
 
-parse = fixStart . neighbours . chunks . lines
+type Node = (Int, Int)
+
+parse :: String -> (Node, M.Map Node (Node, Node))
+parse = ensureStart . neighbours . chunks . lines
   where
     chunks ls = let g = ground ls in zip3 (g : ls) ls (drop 1 ls ++ [g])
     ground (h:_) = length h `replicate` '.'
     enum = zip [0..]
     neighbours ck = foldl f (Nothing, M.empty) (enum ck) where
       f m (y, (p, c, n)) = foldl g m (enum c) where
-          g r     (x, '.') = r
-          g (_, m) (x, 'S') = let k = (x, y) in (Just k, m)
+          g r      (x, '.') = r
+          g (_, m) (x, 'S') = let k = (x, y) in
+            (Just k, M.insert k (neighboursOfStart p c n y x) m)
           g (s, m) (x,  i ) = let k = (x, y) in (s, M.insert k (neighbour i k) m)
     neighbour '|' p = (north p, south p)
     neighbour '-' p = (east p, west p)
@@ -23,7 +28,14 @@ parse = fixStart . neighbours . chunks . lines
     south (x,y) = (x,y+1)
     east (x,y) = (x-1,y)
     west (x,y) = (x+1,y)
-    fixStart (Nothing, _) = error "input does not contain a start node"
-    fixStart (Just s, m) = (s, m) -- TODO
+    neighboursOfStart p c n y x = case nos' p c n y x of
+        [a,b] -> (a, b)
+        [a] -> (a, (x+1,y))
+    nos' p c n y x = catMaybes [
+        if p !! x `elem` "|F7" then Just (x,y-1) else Nothing,
+        if n !! x `elem` "|LJ" then Just (x,y+1) else Nothing,
+        if x > 0 && c !! (x-1) `elem` "-LF" then Just (x-1, y) else Nothing]
+    ensureStart (Just s, m) = (s, m)
+    ensureStart _ = error "input does not contain a start node"
 
 p1 = id
