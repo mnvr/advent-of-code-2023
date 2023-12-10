1c98ada7c1c13834570fe4099e0ad30583aac567
Manav <manav@mrmr.io>
2023-12-10 10:49:46 +0530

    Handle boundary lines

--- 10.hs
+++ 10.hs
@@ -1,4 +1,6 @@
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . lines
 
-p1 ls = zip3 ls (drop 1 ls) (drop 2 ls)
+p1 ls = let g = ground ls in zip3 (g : ls) ls (drop 1 ls ++ [g])
+
+ground (h:_) = length h `replicate` '.'
