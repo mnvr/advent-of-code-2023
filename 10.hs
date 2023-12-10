8236db298964b55b3293f672a07f5cfeafea3f56
Manav <manav@mrmr.io>
2023-12-10 11:09:47 +0530

    Parse

--- 10.hs
+++ 10.hs
@@ -1,6 +1,14 @@
+import Data.Map qualified as M
+
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . lines
 
-p1 ls = let g = ground ls in zip3 (g : ls) ls (drop 1 ls ++ [g])
-
-ground (h:_) = length h `replicate` '.'
+p1 = neighbours . chunks where
+  chunks ls = let g = ground ls in zip3 (g : ls) ls (drop 1 ls ++ [g])
+  ground (h:_) = length h `replicate` '.'
+  enum = zip [0..]
+  neighbours ck = foldl f M.empty (enum ck) where
+    f m (y, (p,c,n)) = foldl g m (enum c) where
+        g m (x, s) = let pos = (x, y) in M.insert pos (neighbour s pos) m
+  neighbour '|' (x, y) = [(x,y-1), (x,y+1)]
+  neighbour _ (x, y) = [(x,y-1), (x,y+1)]
