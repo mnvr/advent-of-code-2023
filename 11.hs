21f0d623c577657c4bf5b798e45333c33f74117a
Manav <manav@mrmr.io>
2023-12-11 22:40:18 +0530

    Scaffold p2

--- 11.hs
+++ 11.hs
@@ -1,12 +1,13 @@
 import Data.List
+import Control.Arrow ((&&&))
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parse
+main = interact $ (++ "\n") . show . (p1 &&& p2) . parse
 
 type Galaxy = (Int, Int)
 
 parse :: String -> [Galaxy]
-parse = expand . concatMap (uncurry row) . enum . lines
+parse = concatMap (uncurry row) . enum . lines
   where
     row y = foldl item []. enum
       where item gs (x, '#') = (y, x) : gs
@@ -15,14 +16,14 @@ parse = expand . concatMap (uncurry row) . enum . lines
 enum :: [a] -> [(Int, a)]
 enum = zip [0..]
 
-expand :: [Galaxy] -> [Galaxy]
-expand gs = sort $ map f gs
+expand :: Int -> [Galaxy] -> [Galaxy]
+expand by gs = sort $ map f gs
   where
     ys = nub (map fst gs)
     xs = nub (map snd gs)
-    missingY = sort [0..maximum ys] \\ ys
-    missingX = [0..maximum xs] \\ xs
-    f (y, x) = (y + adjustY, x + adjustX)
+    missingY = sort $ [0..maximum ys] \\ ys
+    missingX = sort $ [0..maximum xs] \\ xs
+    f (y, x) = (y + (by * adjustY), x + (by * adjustX))
       where
         adjustY = maybe (length missingY) id (findIndex (> y) missingY)
         adjustX = maybe (length missingX) id (findIndex (> x) missingX)
@@ -31,10 +32,14 @@ pairs :: [a] -> [(a, a)]
 pairs gs = concatMap f [0..length gs - 1]
   where f i = let u = gs !! i in map (\v -> (u, v)) (drop (i + 1) gs)
 
-
 dist :: Galaxy -> Galaxy -> Int
 dist (y, x) (y', x') = abs (y - y') + abs (x - x')
 
-
 p1 :: [Galaxy] -> Int
-p1 = sum . map (uncurry dist) . pairs
+p1 = sumD . expand 1
+
+p2 :: [Galaxy] -> Int
+p2 = sumD . expand 10
+
+sumD :: [Galaxy] -> Int
+sumD = sum . map (uncurry dist) . pairs
