dba6ad160f115d629749d2507abad803cb3bde54
Manav <manav@mrmr.io>
2023-12-12 12:08:18 +0530

    A sketch of an approach

--- 12.hs
+++ 12.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-all #-}
+
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . head . parse
 
@@ -9,4 +10,18 @@ parse = map line . lines
         [s, num] -> (s, map read $ words $ map comma num)
     comma c = if c == ',' then ' ' else c
 
-p1 = id
+p1 = go
+  where
+    go (s, xs) = let as = arrangements s in map (\a -> (a, satisfies xs a)) as
+    arrangements :: String -> [String]
+    arrangements [] = []
+    arrangements "?" = [".", "#"]
+    arrangements [c] = [[c]]
+    arrangements ('?':cs) = concatMap (\a -> ['.': a, '#':a]) (arrangements cs)
+    arrangements (c:cs) = map (\a -> c: a) (arrangements cs)
+    satisfies ::  [Int] -> String -> Bool
+    satisfies [] _ = True
+    satisfies _ [] = False
+    satisfies xs ('.':rs) = satisfies xs rs
+    satisfies xs@(x:r) s =
+        if take x s == x `replicate` '#' then satisfies r (drop x s) else False
