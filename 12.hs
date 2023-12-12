638160bcddc5990945ba38670f5cf9ad0d926f1d
Manav <manav@mrmr.io>
2023-12-12 11:44:50 +0530

    Parse

--- 12.hs
+++ 12.hs
@@ -1,7 +1,12 @@
 {-# OPTIONS_GHC -Wno-all #-}
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parse
+main = interact $ (++ "\n") . show . p1 . head . parse
 
-parse = (head) . lines
+parse :: String -> [(String, [Int])]
+parse = map line . lines
+  where
+    line l = case words l of
+        [s, num] -> (s, map read $ words $ map comma num)
+    comma c = if c == ',' then ' ' else c
 
 p1 = id
