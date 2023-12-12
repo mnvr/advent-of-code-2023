7e4984d756d0c8843724aabbecb7379962db22f0
Manav <manav@mrmr.io>
2023-12-12 19:59:10 +0530

    Yet another approach

--- 12.wip.hs
+++ 12.wip.hs
@@ -1,7 +1,9 @@
+{-# OPTIONS_GHC -Wno-all #-}
+
 import Data.List (intercalate)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parse
+main = interact $ (++ "\n") . p1 . parse
 
 parse :: String -> [([String], [Int])]
 parse = map line . lines
@@ -12,14 +14,33 @@ parse = map line . lines
     comma c = if c == ',' then ' ' else c
 
 -- Set this to 1 to toggle between p1 and p2
-rc = 5 :: Int
+rc = 1 :: Int
 
 unfold :: [String] -> [String]
 unfold [s, xs] = [intercalate "?" (replicate rc s), intercalate "," (replicate rc xs)]
 
-p1 :: [([String], [Int])] -> Int
-p1 = sum . map count
+p1' :: [([String], [Int])] -> Int
+p1' = sum . map count
   where count (ss, xs) = sum $ map (`consume` xs) (expand ss)
+
+p1 :: [([String], [Int])] -> [Char]
+p1 zz = unlines $ map f zz
+  where
+    f (s, xs) = g s xs
+    g (a:b) (x:xs)
+      | take 1 a == "#" && a == x `replicate` '#' = g b xs
+      | not (null b) && not (null xs) && take 1 (last b) == "#" && (last b) == (last xs) `replicate` '#' = g (a:unlast b) (x:unlast xs)
+      | length a == x = g b xs
+      | not (null b) && not (null xs) && length (last b) == last xs = g (a:unlast b) (x:unlast xs)
+      | length a > x && '#' `elem` (take x a) = g ((drop (x + 2) a):b) xs
+    g [] [] = show 1
+    g [s] [x] | '#' `notElem` s = show $ length s `nCr` x
+    g u v = "reduced to " ++ show u ++ "  " ++ show v
+
+unlast = reverse . drop 1 . reverse
+
+nCr n r = fact n `div` fact (n - r)
+fact n = product [1..n]
 
 consume :: [String] -> [Int] -> Int
 consume [] [] = 1
