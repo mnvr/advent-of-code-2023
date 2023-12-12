55a09749ff13f050c46048482e1695484fe844c6
Manav <manav@mrmr.io>
2023-12-12 12:33:02 +0530

    Manual memo
    There isn't much of a difference. Interestingly, both these run *much*
    faster when optimized (roughly 20x speedup, from ~10s to 0.5s), though
    that is still not good enough for brute forcing p2.

--- 12.hs
+++ 12.hs
@@ -1,4 +1,5 @@
-{-# OPTIONS_GHC -Wno-all #-}
+import Data.Map qualified as M
+import Data.List (intercalate)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parse
@@ -14,12 +15,6 @@ p1 :: [(String, [Int])] -> Int
 p1 inp = sum (map count inp)
   where
     count (s, xs) = length $ filter (== xs) $ map runs $ arrangements s
-    arrangements :: String -> [String]
-    arrangements [] = []
-    arrangements "?" = [".", "#"]
-    arrangements [c] = [[c]]
-    arrangements ('?':cs) = concatMap (\a -> ['.': a, '#':a]) (arrangements cs)
-    arrangements (c:cs) = map (\a -> c: a) (arrangements cs)
     runs :: String -> [Int]
     runs [] = []
     runs s = case takeWhile (== '#') s of
@@ -27,3 +22,29 @@ p1 inp = sum (map count inp)
         prefix -> let len = length prefix in len : runs (drop len s)
     matchingRuns :: [Int] -> [[Int]] -> Int
     matchingRuns xs = length . filter (== xs)
+
+arrangements :: String -> [String]
+arrangements s = snd $ arr M.empty s
+
+type MemoTable = M.Map String [String]
+
+arr :: MemoTable -> String -> (MemoTable, [String])
+arr m [] = (m, [])
+arr m "?" = (m, [".", "#"])
+arr m [c] = (m, [[c]])
+arr m s = case M.lookup s m of
+      Nothing -> arrDo m s
+      Just r -> (m, r)
+    where
+        arrDo :: MemoTable -> String -> (MemoTable, [String])
+        arrDo m (c:cs) =
+            let (m', r) = arr m cs
+                r' = if c == '?' then concatMap (\a -> ['.': a, '#':a]) r else map (\a -> c: a) r
+            in (M.insert s r' m, r')
+
+p2 :: [(String, [Int])] -> Int
+p2 = p1 . unfold
+
+unfold :: [(String, [Int])] -> [(String, [Int])]
+unfold = map f
+  where f (s, xs) = (intercalate "?" (replicate 5 s), concat (replicate 5 xs))
