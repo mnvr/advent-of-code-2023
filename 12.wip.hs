aa2744c85e00e5fd21be9a6303c1694ba7597b7f
Manav <manav@mrmr.io>
2023-12-12 17:16:39 +0530

    Something

--- 12.wip.hs
+++ 12.wip.hs
@@ -22,14 +22,33 @@ consume ss xs = let r = trace ("consume " ++ show ss ++ " " ++ show xs ++ " call
 
 consume' :: [String] -> [Int] -> Int
 consume' [] [] = 1
-consume' [s] [x] = if matchEx s x then 1 else 0
+consume' [s] [x] | nub s == "#" = if length s == x then 1 else 0
+consume' [s] [x] | otherwise = consume1 s [] [x]
 consume' _ [] = 0
 consume' [] _ = 0
+consume' (s:ss) (x:xs) | nub s == "#" =
+    -- s is a group consisting solely of #, and thus must be entirely consumed
+    if length s /= x then 0 else consume ss xs
+consume' (s:ss) (x:xs) | nub s == "?" =
+    -- s is a group consisting solely of ?, so we can
+    let a = consume ss (x:xs)    -- skip it entirely
+        b = consume1 s ss (x:xs) -- or consume it entirely or partially
+    in a + b
 consume' (s:ss) (x:xs) =
-    let a = consume ss (x:xs)
-        b = consume (s:ss) xs
-        c = consume ss xs
-    in if matchEx s x then a + b + c else 0
+    -- s is a group consisting of a mix of # and ?, so we must consume it at
+    -- least partially (if not entirely).
+    consume1 s ss xs
+
+consume1 s ss xs =
+    -- create all variations of s by substituting either a '.' or a '#' for each
+    -- '?'. Match the resultant words normally. Sum all possible scenarios
+    let yss = map words (variations s)
+    in sum $ map (\ys -> consume (ys ++ ss) xs) yss
+
+variations :: String -> [String]
+variations [] = [[]]
+variations ('#':s) = map ('#':) (variations s)
+variations ('?':s) = concatMap (\s -> ['#':s, ' ':s]) (variations s)
 
 matchEx :: String -> Int -> Bool
 matchEx s x = length s == x
