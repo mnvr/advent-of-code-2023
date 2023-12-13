f7d7e787fab62bf0b67bff11a353274432d506f5
Manav <manav@mrmr.io>
2023-12-13 13:04:51 +0530

    Enumerate variations of the example

--- 13.hs
+++ 13.hs
@@ -1,7 +1,11 @@
+{-# OPTIONS_GHC -Wno-all #-}
+
 import Data.List
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . patterns
+-- main = interact $ (++ "\n") . show . p1 . patterns
+main = interact $ (++ "\n") . p2 . patterns
+-- main = interact $ (++ "\n") . p2e . patterns
 
 type Pattern = [String]
 
@@ -34,3 +38,32 @@ mrow p = go 1 (length p)
       | i == n = 0
       | uncurry mirror (splitAt i p) = i
       | otherwise = go (i + 1) n
+
+
+-- p2e :: [[String]] -> [Char]
+p2e ps = let t2 = (head (drop 1 ps))
+ in concatMap (showP t2) ([t2] ++ (variations t2))
+ where showP p v = unlines (zipWith (++) (zipWith (++) p (repeat "\t")) v) ++ "\n"
+
+
+-- p2 :: [Pattern] -> [Int]
+p2 ps = concatMap (\(j, p) -> concat $ map (\(i, s, p) -> "variation " ++ show i ++ " of pattern " ++ show j ++ " with sum " ++ show s ++ "\n" ++ unlines p) $ filter (\(_,s,_) -> s > 0) $ map (\(i, p') -> (i, frow p', p')) $ zip [1..] (variations p)) $ zip [1..] ps
+-- p2 (p:_) = concat $ map (\(i, s, p) -> "variation " ++ show i ++ " with sum " ++ show s ++ "\n" ++ unlines p) $ filter (\(_,s,_) -> s > 0) $ map (\(i, p') -> (i, frow p', p')) $ zip [0..] (variations p)
+--   where
+    -- f p = filter (> 0) $ map (\p -> p1 [p]) (variations p)
+
+frow p = (mrow p) * 100
+
+-- Brute force enumeration
+variations :: Pattern -> [Pattern]
+variations p = [opposite p y x | y <- [0..length p - 1], x <- [0..length (p !! y) - 1]]
+  where
+    opposite :: Pattern -> Int -> Int -> Pattern
+    opposite p y x = let (up, rest) = splitAt y p
+                         ([row], down) = splitAt 1 rest
+                         (left, rrow) = splitAt x row
+                         ([c], right) = splitAt 1 rrow
+                     in up ++ [left ++ ((opp c) : right)] ++ down
+    opp :: Char -> Char
+    opp '.' = '#'
+    opp '#' = '.'
