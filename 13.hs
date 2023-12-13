35a3a1ef2d1fc76a9571660ad69808cf97ef67ae
Manav <manav@mrmr.io>
2023-12-13 11:44:46 +0530

    Trivial find of mirroring column

--- 13.hs
+++ 13.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . patterns
 
@@ -10,4 +12,14 @@ patterns = reverse .  last . foldl f ([], []) . lines
     f (p, ps) line = (line:p, ps)
     last (p, ps) = if null p then ps else reverse p : ps
 
-p1 = id
+p1 = map rcol
+
+rcol :: Pattern -> Maybe Int
+rcol p = go 1 (ncol p)
+  where
+    ncol (h:_) = length h
+    go i n
+      | i == n = Nothing
+      | all (uncurry mirror) (map (\line -> splitAt i line) p) = Just i
+      | otherwise = go (i + 1) n
+    mirror l r = let n = min (length l) (length r) in take n (reverse l) == take n r
