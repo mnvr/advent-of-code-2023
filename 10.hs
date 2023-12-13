7082391603e2e0050e5e119bb9a144b828e44b5d
Manav <manav@mrmr.io>
2023-12-13 05:39:38 +0530

    Use a list througout

--- 10.hs
+++ 10.hs
@@ -10,7 +10,7 @@ type Node = (Int, Int)
 
 data Parsed = Parsed {
   start :: Node,
-  neighbours :: M.Map Node (Node, Node),
+  neighbours :: M.Map Node [Node],
   dm :: M.Map Node Int,
   ny :: Int,
   nx :: Int
@@ -21,29 +21,29 @@ parse = mkParsed . chunks . lines
   where
     mkParsed ck@((h, _, _):_) = let (s, nb) = ensureStart (neighbours ck)
       in Parsed { start = s, neighbours = nb,
-                  dm = dist s (M.map (\p -> [fst p, snd p]) nb),
+                  dm = dist s nb,
                   ny = length ck, nx = length h }
+
     chunks ls = let g = ground ls in zip3 (g : ls) ls (drop 1 ls ++ [g])
     ground (h:_) = length h `replicate` '.'
-    enum = zip [0..]
+
     neighbours ck = foldl f (Nothing, M.empty) (enum ck) where
-      f m (y, (p, c, n)) = foldl g m (enum c) where
-          g :: (Maybe Node, M.Map Node (Node, Node)) -> (Int, Char) -> (Maybe Node, M.Map Node (Node, Node))
-          g r      (x, '.') = r
+      f m (y, ck@(_, c, _)) = foldl g m (enum c) where
+          g r (_, '.') = r
           g (_, m) (x, 'S') = let k = (y, x) in
-            (Just k, case neighboursOfStart (p, c, n) 'S' k of
-                (Just n1, Just n2) -> M.insert k (n1, n2) m)
-          g (s, m) (x,  i ) = let k = (y, x) in
-            case neighbour (p, c, n) i k of
-                (Just n1, Just n2) -> (s, M.insert k (n1, n2) m)
-                _ -> (s, m)
-    neighbour :: (String, String, String) -> Char -> (Int, Int) -> (Maybe (Int, Int), Maybe (Int, Int))
-    neighbour ck '|' k = (north ck k, south ck k)
-    neighbour ck '-' k = (west ck k, east ck k)
-    neighbour ck 'L' k = (north ck k, east ck k)
-    neighbour ck 'J' k = (north ck k, west ck k)
-    neighbour ck '7' k = (south ck k, west ck k)
-    neighbour ck 'F' k = (south ck k, east ck k)
+            (Just k, M.insert k (neighboursOfStart ck 'S' k) m)
+          g (s, m) (x,  i ) =
+            let k = (y, x)
+                nb = (neighbour ck i k)
+            in if null nb then (s, m) else (s, M.insert k nb m)
+
+    neighbour ck '|' k = catMaybes [north ck k, south ck k]
+    neighbour ck '-' k = catMaybes [west ck k, east ck k]
+    neighbour ck 'L' k = catMaybes [north ck k, east ck k]
+    neighbour ck 'J' k = catMaybes [north ck k, west ck k]
+    neighbour ck '7' k = catMaybes [south ck k, west ck k]
+    neighbour ck 'F' k = catMaybes [south ck k, east ck k]
+
     north (p, _, _) (y, x)
       | p !! x `elem` "|F7S" = Just (y - 1, x)
       | otherwise = Nothing
@@ -57,12 +57,14 @@ parse = mkParsed . chunks . lines
       | x + 1 < length c && c !! (x + 1) `elem` "-J7S" = Just (y, x + 1)
       | otherwise = Nothing
 
-    neighboursOfStart :: (String, String, String) -> Char -> (Int, Int) -> (Maybe (Int, Int), Maybe (Int, Int))
-    neighboursOfStart ck _ key = case catMaybes [
-        north ck key, south ck key, west ck key, east ck key] of
-        [a, b] -> (Just a, Just b)
+    neighboursOfStart ck _ key = catMaybes [
+        north ck key, south ck key, west ck key, east ck key]
+
     ensureStart (Just s, m) = (s, m)
     ensureStart _ = error "input does not contain a start node"
+
+enum :: [a] -> [(Int, a)]
+enum = zip [0..]
 
 dist :: Node -> M.Map Node [Node] -> M.Map Node Int
 dist start neighbours = relax (M.singleton start 0) [start]
@@ -94,7 +96,6 @@ mkGridMap = foldl f M.empty . enum
   where
     f m (y, row) = foldl (g y) m (enum row)
     g y m (x, c) = M.insert (y, x) c m
-    enum = zip [0..]
 
 expand :: Parsed -> Grid
 expand Parsed { neighbours, dm, ny, nx } =
@@ -103,14 +104,16 @@ expand Parsed { neighbours, dm, ny, nx } =
     keys = M.keys dm
     eny = 3 * (ny + 2)
     enx = 3 * (nx + 2)
-    expandLines = addBoundaryLines $ concatMap (addBoundary . expandRow) [0..ny-1]
+    expandLines =
+      addBoundaryLines $ concatMap (addBoundary . expandRow) [0..ny-1]
     expandRow y =
       foldr (\(c1, c2, c3) ([l1, l2, l3]) -> [c1 ++ l1, c2 ++ l2, c3 ++ l3])
       [[], [], []] (expandRow' y)
     expandRow' y = map (\x -> expandCell (y, x)) [0..nx-1]
-    expandCell key | key `elem` keys = expandCell' key (M.lookup key neighbours)
-                   | otherwise = ("???", "???", "???")
-    expandCell' key@(y, x) (Just (n1, n2))
+    expandCell key
+      | key `elem` keys = expandCell' key (M.lookup key neighbours)
+      | otherwise = ("???", "???", "???")
+    expandCell' key@(y, x) (Just [n1, n2])
       | n1 == (y, x - 1) && n2 == (y, x + 1) = ("???", "---", "???")
       | n1 == (y - 1, x) && n2 == (y, x - 1) = ("?|?", "-J?", "???")
       | n1 == (y + 1, x) && n2 == (y, x - 1) = ("???", "-7?", "?|?")
@@ -138,7 +141,8 @@ inverseMap Grid { gm, gny, gnx } = go
     ifEmpty key | isEmpty key = Just key
                 | otherwise = Nothing
     isInBounds (y, x) = y >= 0 && y < gny && x >= 0 && x < gnx
-    isEmpty key = let ch = M.lookup key gm in isInBounds key && ((ch == Just '?') || (ch == Just '#'))
+    isEmpty key = let ch = M.lookup key gm in
+      isInBounds key && ((ch == Just '?') || (ch == Just '#'))
 
 mask :: Grid -> M.Map Node Int -> Grid
 mask Grid { gm, gny, gnx } dm = Grid { gm = go, gny, gnx }
