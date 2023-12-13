f48a26e41f02d9c0bee7a9989f634b02b4dde754
Manav <manav@mrmr.io>
2023-12-13 18:42:05 +0530

    Find alternative row

--- 13.hs
+++ 13.hs
@@ -1,8 +1,9 @@
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), asum)
 import Control.Arrow ((&&&))
 import Data.List
 import Data.Maybe (fromJust)
 import Numeric
+import Data.Bits
 
 main :: IO ()
 main = interact $ (++ "\n") . show . p1 . parse
@@ -28,4 +29,14 @@ rIndex xs = find f [1..length xs - 1]
 
 p1 :: [Pattern] -> Int
 p1 = sum . map (fromJust . ri)
-  where ri (rows, cols) = (*100) <$> rIndex rows <|> rIndex cols
+  where ri Pattern { rows, cols } = (*100) <$> rIndex rows <|> rIndex cols
+
+findAlternative :: Pattern -> Int
+findAlternative Pattern { rows, cols, ncol } = fromJust rf
+  where
+    or = rIndex rows
+    oc = rIndex cols
+    rf = asum $ filter (/= or) $ map rIndex variants
+    variants = [flip y x | y <- [0..length rows - 1], x <- [0..ncol - 1]]
+    flip y x = zipWith (\i r -> if i == y then flipBit x r else r) [0..] rows
+    flipBit x n = n `complementBit` x
