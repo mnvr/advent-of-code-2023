eddf95d71dc26e54a8df157c0e0112ef6f116171
Manav <manav@mrmr.io>
2023-12-14 11:33:04 +0530

    Start counting

--- 14.hs
+++ 14.hs
@@ -4,9 +4,13 @@ import Data.Bifunctor
 import Debug.Trace
 
 main :: IO ()
-main = interact $ (++ "\n") . unlines . parse
+main = interact $ (++ "\n") . show . parse
 
-parse =  transpose . map id . map f . transpose . lines
+parse = sum . map g . countdown . transpose . map f . transpose . lines
  where
     f [] = []
     f s = let (a, b) = span (/= '#') s in (reverse $ sort a) ++ (let (c, d) = break (/= '#') b in c ++ (if null d then [] else f d))
+    countdown xs = zip [length xs..] xs
+    -- g (i, s) = (i, (length $ filter (== 'O') s), s)--unt id -- g' (length ls) ls
+    g (i, s) = i * (length $ filter (== 'O') s)--unt id -- g' (length ls) ls
+    -- g' n ls = map (h n) ls
