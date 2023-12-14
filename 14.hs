7191509760c386ab2e600e75792cba1213570917
Manav <manav@mrmr.io>
2023-12-14 11:17:27 +0530

    p1 example

--- 14.hs
+++ 14.hs
@@ -1,9 +1,12 @@
 import Control.Arrow ((&&&))
 import Data.List
+import Data.Bifunctor
+import Debug.Trace
 
 main :: IO ()
 main = interact $ (++ "\n") . unlines . parse
 
-parse = transpose . map f . transpose . lines
+parse =  transpose . map id . map f . transpose . lines
  where
-    f = id
+    f [] = []
+    f s = let (a, b) = span (/= '#') s in (reverse $ sort a) ++ (let (c, d) = break (/= '#') b in c ++ (if null d then [] else f d))
