5f69fffa9dd90f2ac82f3ba8441c13a0d1d6c7e4
Manav <manav@mrmr.io>
2023-12-14 14:55:37 +0530

    Works on p2 example

--- 12.wip.t2.hs
+++ 12.wip.t2.hs
@@ -1,10 +1,8 @@
-{-# OPTIONS_GHC -Wno-all #-}
-
-import Data.List (nub)
+import Data.List (nub, intercalate)
 import Debug.Trace (trace)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parse
+main = interact $ (++ "\n") . show . p2 . parse
 
 parse :: String -> [(String, [Int])]
 parse = map line . lines
@@ -13,8 +11,13 @@ parse = map line . lines
         [s, num] -> (s, map read $ words $ map comma num)
     comma c = if c == ',' then ' ' else c
 
--- p1 :: [(String, [Int])] -> Int
+p1, p2 :: [(String, [Int])] -> Int
 p1 = sum . map (uncurry ways)
+p2 = p1 . unfold
+
+unfold :: [(String, [Int])] -> [(String, [Int])]
+unfold = map f
+  where f (s, xs) = (intercalate "?" (replicate 5 s), concat (replicate 5 xs))
 
 ways :: String -> [Int] -> Int
 ways = ways' ways
