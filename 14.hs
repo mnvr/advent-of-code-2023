3230964db743bc0cfd8339f6e68401dc3225e4c0
Manav <manav@mrmr.io>
2023-12-14 12:02:50 +0530

    Cleanup

--- 14.hs
+++ 14.hs
@@ -1,35 +1,33 @@
 import Control.Arrow ((&&&))
 import Data.List
-import Debug.Trace
 
 main :: IO ()
-main = interact $ (++ "\n") . p2 . lines
+main = interact $ (++ "\n") . show . (p1 &&& p2) . lines
+-- main = interact $ (++ "\n") . p1 . lines
 
-north :: [String] -> [String]
+north, south, east, west, cycle1, cycle1B :: [String] -> [String]
 north = transpose . map f . transpose
-
-f s = let (a, b) = span (/= '#') s in (reverse $ sort a) ++ (let (c, d) = break (/= '#') b in c ++ (if null d then [] else f d))
-
 west = map f
 south = transpose . map reverse . map f . map reverse . transpose
 east = map reverse . map f . map reverse
--- sounwest = map f
-
 cycle1 = east . south . west . north
+cycle1B = (\(h:_) -> h) . drop 3 . iterate cycle1
+-- cycle1B = (\(h:_) -> h) . drop 1000000000 . iterate cycle1
 
+f :: String -> String
+f s = let (a, b) = span (/= '#') s
+      in (reverse $ sort a) ++ (
+        let (c, d) = break (/= '#') b in c ++ (
+            if null d then [] else f d))
+
+load :: [String] -> Int
 load = sum . map g . countdown
  where
-    countdown xs = let n = length xs in zip [n,n-1..] xs
+    countdown xs = let n = length xs in zip [n, n-1 ..] xs
     g (i, s) = i * (length $ filter (== 'O') s)
 
-p1 :: [String] -> Int
+p1, p2 :: [String] -> Int
 p1 = load . north
- where
-    countdown xs = let n = length xs in zip [n,n-1..] xs
-    g (i, s) = i * (length $ filter (== 'O') s)
+p2 = load . cycle1B
 
--- p2 = load . cycle10k
-p2 = unlines . cycle10k
-
-cycle10k = (\(h:_) -> h) . drop 3 . iterate cycle1
--- cycle10k = (\(h:_) -> h) . drop 1000000000 . iterate cycle1
+p2v = unlines . cycle1B
