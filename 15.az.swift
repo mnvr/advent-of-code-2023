bc7992c4d11c5eecd3a0446dfc8cbe638177b127
Manav <manav@mrmr.io>
2023-12-15 16:18:15 +0530

    Go further

--- 15.az.swift
+++ 15.az.swift
@@ -2,43 +2,32 @@ var s = 0
 var boxes: [[[String]]] = Array(repeating: [], count: 256)
 while let line = readLine() {
     for word in line.split(separator: ",") {
-        let splits = word.split { $0 == "=" || $0 == "-" } .map { String($0) }
+        let splits = word.split { "=-".contains($0) } .map { String($0) }
         let bi = hash(splits[0])
         s += hash(String(word))
-        boxes[bi] = modify(box: boxes[bi], splits, bi)
+        modify(box: &boxes[bi], splits, bi)
     }
 }
 print(s, power(boxes))
 
 func hash(_ s: String) -> Int {
-    return s.utf8.reduce(0) { ($0 + Int($1)) * 17 % 256 }
+    s.utf8.reduce(0) { ($0 + Int($1)) * 17 % 256 }
 }
 
-func modify(box: [[String]], _ splits: [String], _ bi: Int) -> [[String]] {
-    var box = box
-    var label = splits.first
-    if splits.count == 1 {
-        box.removeAll { $0.first == label }
-    } else {
+func modify(box: inout [[String]], _ splits: [String], _ bi: Int) {
+    if splits.count == 1 { box.removeAll { $0[0] == splits[0] } }
+    else {
         for (i, l) in box.enumerated() {
-            if l.first == label {
-                box[i] = splits
-                label = nil
-            }
+            if l[0] == splits[0] { return box[i] = splits  }
         }
-        if let _ = label {
-            box.append(splits)
-        }
+        box.append(splits)
     }
-    return box
 }
 
 func power(_ boxes: [[[String]]]) -> Int {
-    var s = 0
-    for (i, box) in boxes.enumerated() {
-        for (j, lens) in box.enumerated() {
-            s += (i + 1) * (j + 1) * Int(lens.last!, radix: 10)!
+    boxes.enumerated().reduce(0) { s, b in
+        b.1.enumerated().reduce(s) {
+            $0 + (b.0 + 1) * ($1.0 + 1) * Int($1.1.last!)!
         }
     }
-    return s
 }
