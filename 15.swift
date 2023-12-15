0116c6bda48eeed99b3a1156854b78590b77141e
Manav <manav@mrmr.io>
2023-12-15 11:40:00 +0530

    HASH

--- 15.swift
+++ 15.swift
@@ -1,8 +1,12 @@
+var v = 0
 while let line = readLine() {
     for word in line.split(separator: " ") {
-        for v in word.utf8 {
-            print(v)
+        for c in word.utf8 {
+            print(c, v)
+            v += Int(c)
+            v *= 17
+            v %= 256
         }
     }
-    print(line, line.count)
+    print(line, line.count, v)
 }
