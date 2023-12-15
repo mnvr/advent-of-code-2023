86b8a3f148756be4388804edfda295ed98c5f865
Manav <manav@mrmr.io>
2023-12-15 14:22:06 +0530

    Haskell p2 modify

--- 15.hs
+++ 15.hs
@@ -16,22 +16,29 @@ hash :: String -> Int
 hash = foldl (\v c -> (v + (ord c)) * 17 `mod` 256) 0
 
 type Lens = (String, Int)
-data Op = Remove | Replace Lens deriving Show
+data Op = Remove String | Replace Lens
 type Step = (Op, Int)
 
 decode :: String -> Step
 decode s = case break (`elem` "-=") s of
-    (a, '-':b) -> (Remove, hash a)
+    (a, '-':b) -> (Remove a, hash a)
     (a, '=':b) -> (Replace (a, read b), hash a)
 
 -- p2 :: [String] -> Int
--- p2 = foldl f boxes
--- p2 :: [String] -> [[Int]]
--- p2 :: [String] -> [[Int]]
-p2 = map decode
--- p2 = foldl f boxes . map decode
---   where
---     boxes = take 256 $ repeat []
---     f bs = (`modify` bs)
---     modify st = zipWith (modifyBox st) [1..]
---     modifyBox st = zipWith ( st)
+p2 = filter (not . null) . foldl f boxes . map decode
+  where
+    boxes = take 256 $ repeat []
+    f bs = (`modify` bs)
+    modify step = zipWith (modifyBox step) [0..]
+    modifyBox (op, si) i box
+      | si == i = modifyBox' op box
+      | otherwise = box
+    modifyBox' (Remove label) = filter ((/= label) . fst)
+    modifyBox' (Replace lens) = reverse . append . foldl g (Just lens, [])
+      where
+        g (Nothing, box) lens' = (Nothing, lens' : box)
+        g (Just lens, box) lens'
+              | (fst lens) == (fst lens') = (Nothing, lens : box)
+              | otherwise = (Just lens, lens' : box)
+        append (Nothing, box) = box
+        append (Just lens, box) = lens : box
