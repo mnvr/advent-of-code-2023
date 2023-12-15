741cab0790ff87ed2ca43b388275ec78c1a41b02
Manav <manav@mrmr.io>
2023-12-15 11:37:46 +0530

    Read input

--- 15.swift
+++ 15.swift
@@ -1,3 +1,8 @@
 while let line = readLine() {
-    print(line)
+    for word in line.split(separator: " ") {
+        for v in word.utf8 {
+            print(v)
+        }
+    }
+    print(line, line.count)
 }
