78439db0a4f4e254c268b08c7102cebf483839ed
Manav <manav@mrmr.io>
2023-12-15 12:31:57 +0530

    Decode for p2

--- 15.swift
+++ 15.swift
@@ -2,9 +2,10 @@ var s = 0
 while let line = readLine() {
     for word in line.split(separator: ",") {
         s += hash(word)
+        print(decode(word))
     }
 }
-print(s)
+// print(s)
 
 func hash<S: StringProtocol>(_ s: S) -> Int {
     var v = 0;
@@ -14,4 +15,29 @@ func hash<S: StringProtocol>(_ s: S) -> Int {
         v %= 256
     }
     return v;
+}
+
+enum Op {
+    case remove
+    case replace(Int)
+}
+
+struct Action {
+    let box: Int
+    let label: String
+    let op: Op
+
+    init<S: StringProtocol>(label: S, op: Op) {
+        self.box = hash(label)
+        self.label = String(label)
+        self.op = op
+    }
+}
+
+func decode<S: StringProtocol>(_ s: S) -> Action {
+    let splits = s.split { $0 == "=" || $0 == "-" }
+    if splits.count == 1 {
+        return Action(label: splits[0], op: .remove)
+    }
+    return Action(label: splits[0], op: .replace(Int(splits[1], radix: 10)!))
 }
