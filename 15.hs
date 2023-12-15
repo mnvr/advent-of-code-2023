1ab7f30a1b597d75bd3572766e264befc4b6a166
Manav <manav@mrmr.io>
2023-12-15 14:06:05 +0530

    Haskell p2 parse

--- 15.hs
+++ 15.hs
@@ -1,7 +1,7 @@
 import Data.Char (ord)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . steps . lines
+main = interact $ (++ "\n") . show . p2 . steps . lines
 
 steps :: [String] -> [String]
 steps = concatMap f
@@ -14,3 +14,24 @@ p1 = sum . map hash
 
 hash :: String -> Int
 hash = foldl (\v c -> (v + (ord c)) * 17 `mod` 256) 0
+
+type Lens = (String, Int)
+data Op = Remove | Replace Lens deriving Show
+type Step = (Op, Int)
+
+decode :: String -> Step
+decode s = case break (`elem` "-=") s of
+    (a, '-':b) -> (Remove, hash a)
+    (a, '=':b) -> (Replace (a, read b), hash a)
+
+-- p2 :: [String] -> Int
+-- p2 = foldl f boxes
+-- p2 :: [String] -> [[Int]]
+-- p2 :: [String] -> [[Int]]
+p2 = map decode
+-- p2 = foldl f boxes . map decode
+--   where
+--     boxes = take 256 $ repeat []
+--     f bs = (`modify` bs)
+--     modify st = zipWith (modifyBox st) [1..]
+--     modifyBox st = zipWith ( st)
