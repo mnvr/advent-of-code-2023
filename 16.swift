e5f9ec85bb07afc17f3108e664dbb6d960cac060
Manav <manav@mrmr.io>
2023-12-16 21:28:36 +0530

    Inline

--- 16.swift
+++ 16.swift
@@ -3,7 +3,7 @@ while let line = readLine() {
     grid.append(items(line))
 }
 
-var f = energized(by: Beam(ix: [0, 0], d: .r))
+var f = energized(by: Beam(x: 0, y: 0, d: .r))
 var m = edges().map({ energized(by: $0) }).max() ?? 0
 print(f, m)
 
@@ -11,14 +11,13 @@ enum Item: Character {
     case dot = ".", hbar = "-", vbar = "|", fslash = "/", bslash = "\\"
 }
 
-func items(_ s: String) -> [Item] {  s.compactMap { Item(rawValue: $0) } }
-func item(at beam: Beam) -> Item { grid[beam.ix[1]][beam.ix[0]] }
+func items(_ s: String) -> [Item] { s.compactMap { Item(rawValue: $0) } }
+func item(at beam: Beam) -> Item { grid[beam.y][beam.x] }
 
 enum Direction { case l, r, u, d }
 
 struct Beam: Hashable {
-    // This is an (Int, Int), but Swift doesn't synthesize Hashable for tuples.
-    let ix: [Int]
+    let x, y: Int
     let d: Direction
 
     var isHorizontal: Bool {  d == .l || d == .r }
@@ -26,33 +25,31 @@ struct Beam: Hashable {
 
     var step: Beam {
         switch d {
-            case .l: Beam(ix: [ix[0] - 1, ix[1]], d: d)
-            case .r: Beam(ix: [ix[0] + 1, ix[1]], d: d)
-            case .u: Beam(ix: [ix[0], ix[1] - 1], d: d)
-            case .d: Beam(ix: [ix[0], ix[1] + 1], d: d)
+            case .l: Beam(x: x - 1, y: y, d: d)
+            case .r: Beam(x: x + 1, y: y, d: d)
+            case .u: Beam(x: x, y: y - 1, d: d)
+            case .d: Beam(x: x, y: y + 1, d: d)
         }
     }
 
     var hsplit: [Beam] {
-        [Beam(ix: [ix[0] - 1, ix[1]], d: .l),
-         Beam(ix: [ix[0] + 1, ix[1]], d: .r)]
+        [Beam(x: x - 1, y: y, d: .l),  Beam(x: x + 1, y: y, d: .r)]
     }
 
     var vsplit: [Beam] {
-        [Beam(ix: [ix[0], ix[1] - 1], d: .u),
-         Beam(ix: [ix[0], ix[1] + 1], d: .d)]
+        [Beam(x: x, y: y - 1, d: .u),  Beam(x: x, y: y + 1, d: .d)]
     }
 
-    var reflectL: Beam { Beam(ix: [ix[0] - 1, ix[1]], d: .l) }
-    var reflectR: Beam { Beam(ix: [ix[0] + 1, ix[1]], d: .r) }
-    var reflectU: Beam { Beam(ix: [ix[0], ix[1] - 1], d: .u) }
-    var reflectD: Beam { Beam(ix: [ix[0], ix[1] + 1], d: .d) }
+    var reflectL: Beam { Beam(x: x - 1, y: y, d: .l) }
+    var reflectR: Beam { Beam(x: x + 1, y: y, d: .r) }
+    var reflectU: Beam { Beam(x: x, y: y - 1, d: .u) }
+    var reflectD: Beam { Beam(x: x, y: y + 1, d: .d) }
 }
 
 func energized(by beam: Beam) -> Int {
     var visited = Set<Beam>()
     trace(beam: beam, visited: &visited)
-    return Set(visited.map({$0.ix})).count
+    return Set(visited.map({ [$0.x, $0.y] })).count
 }
 
 func trace(beam: Beam, visited: inout Set<Beam>) {
@@ -92,8 +89,8 @@ func trace(beam: Beam, visited: inout Set<Beam>) {
 }
 
 func isInBounds(_ beam: Beam) -> Bool {
-    let x = beam.ix[0]
-    let y = beam.ix[1]
+    let x = beam.x
+    let y = beam.y
     return x >= 0 && x < grid[0].count && y >= 0 && y < grid.count
 }
 
@@ -103,12 +100,12 @@ func edges() -> [Beam] {
 
     var result: [Beam] = []
     for y in 0..<ny {
-        result.append(Beam(ix: [0, y], d: .r))
-        result.append(Beam(ix: [nx - 1, y], d: .l))
+        result.append(Beam(x: 0, y: y, d: .r))
+        result.append(Beam(x: nx - 1, y: y, d: .l))
     }
     for x in 0..<nx {
-        result.append(Beam(ix: [x, 0], d: .d))
-        result.append(Beam(ix: [x, ny - 1], d: .u))
+        result.append(Beam(x: x, y: 0, d: .d))
+        result.append(Beam(x: x, y: ny - 1, d: .u))
     }
     return result
 }
