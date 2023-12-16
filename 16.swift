ea580ef1623316e2ac853fa1a39c8888910c3633
Manav <manav@mrmr.io>
2023-12-16 21:19:24 +0530

    Tinker

--- 16.swift
+++ 16.swift
@@ -1,4 +1,4 @@
-var grid: Grid = []
+var grid: [[Item]] = []
 while let line = readLine() {
     grid.append(items(line))
 }
@@ -11,18 +11,18 @@ enum Item: Character {
     case dot = ".", hbar = "-", vbar = "|", fslash = "/", bslash = "\\"
 }
 
-func items(_ s: String) -> [Item] {
-    s.compactMap { Item(rawValue: $0) }
-}
+func items(_ s: String) -> [Item] {  s.compactMap { Item(rawValue: $0) } }
+func item(at beam: Beam) -> Item { grid[beam.ix[1]][beam.ix[0]] }
 
-typealias Grid = [[Item]]
-// This is an (Int, Int), but Swift doesn't synthesize Hashable for tuples.
-typealias Ix = [Int]
 enum Direction { case l, r, u, d }
 
 struct Beam: Hashable {
-    let ix: Ix
+    // This is an (Int, Int), but Swift doesn't synthesize Hashable for tuples.
+    let ix: [Int]
     let d: Direction
+
+    var isHorizontal: Bool {  d == .l || d == .r }
+    var isVertical: Bool {  d == .u || d == .d }
 
     var step: Beam {
         switch d {
@@ -52,15 +52,7 @@ struct Beam: Hashable {
 func energized(by beam: Beam) -> Int {
     var visited = Set<Beam>()
     trace(beam: beam, visited: &visited)
-    return tileCount(visited)
-}
-
-func tileCount(_ visited: Set<Beam>) -> Int {
-    Set(visited.map({$0.ix})).count
-}
-
-func item(at beam: Beam) -> Item {
-    grid[beam.ix[1]][beam.ix[0]]
+    return Set(visited.map({$0.ix})).count
 }
 
 func trace(beam: Beam, visited: inout Set<Beam>) {
@@ -70,12 +62,13 @@ func trace(beam: Beam, visited: inout Set<Beam>) {
 
     var beam = beam
     var next: [Beam]?
+
     while isInBounds(beam) && next == nil {
         visited.insert(beam)
 
         switch item(at: beam) {
-        case .vbar where isHorizontal(beam): next = beam.vsplit
-        case .hbar where isVertical(beam):   next = beam.hsplit
+        case .vbar where beam.isHorizontal: next = beam.vsplit
+        case .hbar where beam.isVertical:   next = beam.hsplit
         case .fslash:
             switch beam.d {
                 case .l: next = [beam.reflectD]
@@ -95,19 +88,7 @@ func trace(beam: Beam, visited: inout Set<Beam>) {
         }
     }
 
-    for n in next ?? [] {
-        if isInBounds(n) {
-            trace(beam: n, visited: &visited)
-        }
-    }
-}
-
-func isHorizontal(_ beam: Beam) -> Bool {
-    beam.d == .l || beam.d == .r
-}
-
-func isVertical(_ beam: Beam) -> Bool {
-    !isHorizontal(beam)
+    next?.filter(isInBounds).forEach { trace(beam: $0, visited: &visited) }
 }
 
 func isInBounds(_ beam: Beam) -> Bool {
@@ -125,7 +106,7 @@ func edges() -> [Beam] {
         result.append(Beam(ix: [0, y], d: .r))
         result.append(Beam(ix: [nx - 1, y], d: .l))
     }
-    for x in  0..<nx {
+    for x in 0..<nx {
         result.append(Beam(ix: [x, 0], d: .d))
         result.append(Beam(ix: [x, ny - 1], d: .u))
     }
