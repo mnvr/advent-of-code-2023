f41a4f096f6c8e9d9570c4767acf7ce6a657630a
Manav <manav@mrmr.io>
2023-12-17 10:55:29 +0530

    Retain size

--- 17.hs
+++ 17.hs
@@ -1,9 +1,19 @@
 import Data.Char (digitToInt)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . id . parse
+main = interact $ (++ "\n") . show . p1 . parse
 
-parse :: String -> [[Int]]
-parse = map (map digitToInt) . lines
+data Grid = Grid { gv :: [[Int]], mx, my :: Int }
 
-p1 = id
+parse :: String -> Grid
+parse = mkG . map (map digitToInt) . lines
+  where mkG xs@(h:_) = Grid xs (length h) (length xs)
+
+p1 = (`path` (0, 0))
+
+path :: Grid -> (Int, Int) -> Int
+path Grid { gv, mx, my } start = go start 0 3
+  where
+    go :: (Int, Int) -> Int -> Int -> Int
+    go (x, y) c _ | (x, y) == (mx, my) = c
+    go (x, y) c t = 0
