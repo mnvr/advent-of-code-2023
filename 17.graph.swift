e96e378fb5644dbc60f90ac39849bf0b1d31a61f
Manav <manav@mrmr.io>
2023-12-17 18:03:13 +0530

    Create a Grid type

--- 17.graph.swift
+++ 17.graph.swift
@@ -8,5 +8,29 @@ func readInput() -> [[Int]] {
     return result
 }
 
+struct Grid<T> {
+    /// Swift doesn't generate Hashable conformance for tuples, so what could've
+    /// been an (Int, Int) is now this.
+    struct Index: Hashable {
+        let x: Int
+        let y: Int
+    }
+
+    let items: [[T]]
+    let maxIndex: Index
+
+    init(items: [[T]]) {
+        self.items = items
+        self.maxIndex = Index(x: items[0].count, y: items.count)
+    }
+}
+
+func dfs<T>(grid: Grid<T>, start: Grid<T>.Index, visit: (T) -> Void) {
+    // var visited = Set<Grid<T>.Index>()
+    print("helol")
+}
+
 let input = readInput()
-print(input)
+let grid = Grid(items: input)
+print(grid)
+dfs(grid: grid, start: .init(x: 0, y: 0)) { print($0) }
