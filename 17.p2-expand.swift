9c7bd6535d6db7c8f77f80db9bbfcba0c041eea5
Manav <manav@mrmr.io>
2023-12-19 06:30:57 +0530

    Show the original grid at the end
    Remove generics for ease, this code is not generic anyway.

--- 17.p2-expand.swift
+++ 17.p2-expand.swift
@@ -46,10 +46,10 @@ let directions: [ComplexInt] = [.north, .south, .east, .west]
 let maxDirection = directions.count - 1
 /// minimum is 4 steps, this is 3 since we start counting from 0
 var minStep = 0 // 3
-minStep = 3
+// minStep = 3
 /// maximum is 10 steps, this is 9 since we start counting from 0
-var maxStep = 2 // 9
-maxStep = 9 // 9
+var maxStep = 9 // 9
+// maxStep = 9 // 9
 
 struct ExpandedItem {
     /// The original item / value
@@ -88,22 +88,23 @@ func expand(items: [[Int]]) -> [[[[ExpandedItem]]]] {
     return expanded4
 }
 
-struct Grid<T> {
+struct Grid {
     struct Index: Hashable {
         let xy: ComplexInt
         let heading: ComplexInt
         let step: Int
     }
 
-    let items: [[[[T]]]]
+    let items: [[Int]]
+    let expandedItems: [[[[ExpandedItem]]]]
     let maxIndex: Index
 
-    init(items: [[[[T]]]]) {
+    init(items: [[Int]], expandedItems: [[[[ExpandedItem]]]]) {
         self.items = items
+        self.expandedItems = expandedItems
         self.maxIndex = Index(
             xy: ComplexInt(x: items[0].count - 1, y: items.count - 1),
-            heading: directions[maxDirection], step: maxStep
-        )
+            heading: directions[maxDirection], step: maxStep)
     }
 
     var totalItems: Int {
@@ -116,9 +117,13 @@ struct Grid<T> {
         u.step >= minStep && u.step <= maxStep
     }
 
-    func at(_ u: Index) -> T {
+    func at(_ u: Index) -> ExpandedItem {
         let hi = directions.firstIndex(of: u.heading)!
-        return items[u.xy.y][u.xy.x][hi][u.step]
+        return expanded[u.xy.y][u.xy.x][hi][u.step]
+    }
+
+    func at(xy: ComplexInt) -> Int {
+        items[xy.y][xy.x]
     }
 
     func adjacent(_ u: Index) -> [Index] {
@@ -140,17 +145,6 @@ struct Grid<T> {
 
     /// Precondition: v must be adjacent to u
     func edgeWeight( from u: Index, to v: Index) -> Int {
-        // All the expanded indices have the same weight, so we can arbitrarily
-        // pick any of them.
-        func weight(xy: ComplexInt) -> Int {
-            let i = Index(xy: xy, heading: .east, step: 0)
-            guard let e = at(i) as? ExpandedItem else { fatalError() }
-            // guard let ex = at(v) as? ExpandedItem else { fatalError() }
-            // print("weight for (\(xy.x), \(xy.y)) is \(e.item), expected \(ex.item) [u = \(u), v = \(v)]")
-            // assert(e.item == ex.item)
-            return e.item
-        }
-
         // It is guarantee that at least u and v will share one of the cartesian
         // coordinates for their positions. Start at u, but move in the heading
         // of v. The edge weight is then the sum of the weights of all the nodes
@@ -159,7 +153,7 @@ struct Grid<T> {
         var w = 0
         repeat {
             t = t + v.heading
-            w += weight(xy: t)
+            w += at(xy: t)
         } while t != v.xy
         // let (ux, uy) = (u.xy.x, u.xy.y)
         // let (vx, vy) = (v.xy.x, v.xy.y)
@@ -193,31 +187,27 @@ struct Grid<T> {
         }
         return result
     }
-
-    func expandMaxIndex() -> [Index] {
-        expandedIndex(xy: maxIndex.xy)
-    }
 }
 
-struct DijkstraState<T> {
-    let grid: Grid<T>
+struct DijkstraState {
+    let grid: Grid
     let iteration: Int
-    let distance: [Grid<T>.Index: Int]
-    let parent: [Grid<T>.Index: Grid<T>.Index]
+    let distance: [Grid.Index: Int]
+    let parent: [Grid.Index: Grid.Index]
 }
 
-typealias Visitor<T> = (DijkstraState<T>) -> Void
+typealias Visitor = (DijkstraState) -> Void
 
 /// Find the shortest path from `start` to all nodes using Dijkstra's algorithm.
-func shortestPath<T>(grid: Grid<T>, start: Grid<T>.Index, visit: Visitor<T>? = nil
-) -> DijkstraState<T> {
+func shortestPath(grid: Grid, start: Grid.Index, visit: Visitor? = nil
+) -> DijkstraState {
     var pending = [start]
-    var visited = Set<Grid<T>.Index>()
+    var visited = Set<Grid.Index>()
     var distance = [start: 0]
-    var parent: [Grid<T>.Index: Grid<T>.Index] = [:]
+    var parent: [Grid.Index: Grid.Index] = [:]
     var iteration = 0
 
-    func state() -> DijkstraState<T> {
+    func state() -> DijkstraState {
         .init(grid: grid, iteration: iteration, distance: distance, parent: parent)
     }
 
@@ -226,7 +216,7 @@ func shortestPath<T>(grid: Grid<T>, start: Grid<T>.Index, visit: Visitor<T>? = n
     // Here we do an (inefficient) simulation using only the standard library
     // data structures. For real programs, consider using a priority queue, like
     // the Heap in the Swift Collections package.
-    func popNearest() -> Grid<T>.Index? {
+    func popNearest() -> Grid.Index? {
         var ui: Int?
         var ud = Int.max
         for (vi, v) in pending.enumerated() {
@@ -261,32 +251,97 @@ func shortestPath<T>(grid: Grid<T>, start: Grid<T>.Index, visit: Visitor<T>? = n
     return state()
 }
 
-func trace(state: DijkstraState<ExpandedItem>) {
+func trace(state: DijkstraState) {
     if state.iteration % 500 == 0 {
         let total = grid.totalItems
         print("iteration \(state.iteration) found tentative distances to \(state.distance.count) / \(total) items")
     }
 }
 
-func ourShortestPath(grid: Grid<ExpandedItem>) -> Int? {
+extension Grid {
+    /// Create string representation of the grid suitable for printing on a
+    /// terminal.
+    func renderToString(
+        state: DijkstraState, start: Grid.Index, end: Grid.Index
+    ) -> String {
+        let tHighlight = "\u{001B}[0;0m"
+        let tDim = "\u{001B}[2;80m"
+        let tReset = "\u{001B}[0m"
+
+        let distance = state.distance
+
+        var selectedPath = Set([start.xy, end.xy])
+        var u = end
+        while let v = state.parent[u] {
+            selectedPath.insert(v.xy)
+            u = v
+        }
+
+        func parent(_ u: Index) -> String? {
+            return nil
+            // guard let p = state.parent[u] else { return nil }
+            // switch(p.xy.x - u.xy.x, p.xy.y - u.xy.y) {
+            //     case (let x, 0) where x < 0: return "→"
+            //     case (let x, 0) where x > 0: return "←"
+            //     case (0, let y) where y < 0: return "↓"
+            //     case (0, let y) where y > 0: return "↑"
+            //     default: return "!"
+            // }
+        }
+
+        var result = [String]()
+        // let maxXY = grid.maxIndex.xy;
+        // for y in 0...maxXY.y {
+        //     for x in 0... {
+        //         let u = Index(x: x, y: y)
+        //         result.append(selectedPath.contains(u) ? tHighlight : tDim);
+        //         if let d = distance[u], let p = parent(u) {
+        //             result.append("\(p) \(item) \(d)\t")
+        //         } else {
+        //             result.append("  \(item)\t")
+        //         }
+        //         result.append(tReset);
+        //     }
+        //     result.append("\n")
+        // }
+        return result.joined()
+    }
+}
+
+func ourShortestPath(grid: Grid) -> Int? {
     func sp(heading: ComplexInt) -> Int? {
-        let topLeft = ComplexInt(x: 0, y: 0)
-        let spState = shortestPath(
-            grid: grid, start: .init(xy: topLeft, heading: .east, step: minStep),
-            visit: trace)
-        // Find the minimum from amongst the distances of the original item
-        let endIndices = grid.expandMaxIndex()
-        let endDistance = endIndices.compactMap { spState.distance[$0] }.min()
+        let startXY = ComplexInt(x: 0, y: 0)
+        let endXY = grid.maxIndex.xy;
+
+        let start = Grid.Index(xy: startXY, heading: .east, step: minStep)
+
+        let state = shortestPath(grid: grid, start: start, visit: trace)
+
+        // Find the minimum from amongst the distances of the original item.
+        let endIndices = grid.expandedIndex(xy: endXY)
+        var end: Grid.Index?
+        var endDistance: Int?
+        for u in endIndices {
+            if let d = state.distance[u], d < (endDistance ?? Int.max) {
+                end = u
+                endDistance = d
+            }
+        }
+        if let end {
+            print(
+                grid.renderToString(state: state, start: start, end: end),
+                terminator: "")
+        }
         return endDistance
     }
 
-    // return [sp(heading: .east)].compactMap({$0}).min()
-    return [sp(heading: .east), sp(heading: .south)].compactMap({$0}).min()
+    // TODO
+    return [sp(heading: .east)].compactMap({$0}).min()
+    //return [sp(heading: .east), sp(heading: .south)].compactMap({$0}).min()
 }
 
 let input = readInput()
 let expanded = expand(items: input)
-let grid = Grid(items: expanded)
-
+let grid = Grid(items: input, expandedItems: expanded)
 let sp = ourShortestPath(grid: grid)
 print("shortest-path-result", sp ?? -1)
