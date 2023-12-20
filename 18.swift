a837e960d26d83f500552000a8962c4aa8012f51
Manav <manav@mrmr.io>
2023-12-20 21:42:00 +0530

    P2 parse

--- 18.swift
+++ 18.swift
@@ -1,19 +1,30 @@
 struct Step {
     let direction: Character
     let count: Int
-    let color: String
 }
 
-func readInput() -> [Step] {
-    var result: [Step] = []
+func readInput() -> ([Step], [Step]) {
+    var s1: [Step] = []
+    var s2: [Step] = []
     while let line = readLine() {
         let splits = line.split(separator: " ")
-        let direction = splits[0].first!
-        let count = Int(splits[1])!
-        let color = String(splits[2])
-        result.append(Step(direction: direction, count: count, color: color))
+
+        var direction = splits[0].first!
+        var count = Int(splits[1])!
+        s1.append(Step(direction: direction, count: count))
+
+        let color = Array(splits[2])
+        direction = switch(color[7]) {
+        case "0": "R"
+        case "1": "D"
+        case "2": "L"
+        case "3": "U"
+        default: fatalError("\(color)")
+        }
+        count = Int(String(color[2...6]), radix:16)!
+        s2.append(Step(direction: direction, count: count))
     }
-    return result
+    return (s1, s2)
 }
 
 /// The interior area of a polygon with integral cartesian coordinates can be
@@ -57,6 +68,7 @@ func area(steps: [Step]) -> Int {
 }
 
 
-let steps = readInput()
-let ar = area(steps: steps)
-print(ar)
+let (s1, s2) = readInput()
+let a1 = area(steps: s1)
+let a2 = area(steps: s2)
+print(a1, a2)
