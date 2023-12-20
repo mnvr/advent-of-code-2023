854c6f81256503b4b31484352a6b9a14df26524b
Manav <manav@mrmr.io>
2023-12-20 21:24:47 +0530

    Guess the relationship - some form of Pick's formula

--- 18.swift
+++ 18.swift
@@ -25,8 +25,10 @@ func readInput() -> [Step] {
 func area(steps: [Step]) -> Int {
     var (px, py) = (0, 0)
     var s = 0
+    var i = 0
+    var b = 0
     for step in steps {
-        print(px, py, s)
+        // print(px, py, s)
         var x, y : Int
         switch step.direction {
         case "R": (x, y) = (px + step.count, py)
@@ -36,15 +38,22 @@ func area(steps: [Step]) -> Int {
         default: fatalError()
         }
         s += (py + y) * (px - x)
+        i += (py + y) * (px - x)
         s += step.count
+        b += step.count
+        // print(step.count, s)
         px = x
         py = y
     }
     // tie it back to the origin
-    // s += (py + 0) * (px - 0)
+    s += (py + 0) * (px - 0)
+    i += (py + 0) * (px - 0)
     // print(px, py, s)
+    // print("end i b", i, b)
 
-    return abs(s) / 2
+    return (i + b + 2) / 2
+
+    // return mabs(s) / 2
 }
 
 
