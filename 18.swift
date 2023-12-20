b2244196e751b0b7c0bb9e439f0e1d4bbb333218
Manav <manav@mrmr.io>
2023-12-20 20:43:53 +0530

    Parse

--- 18.swift
+++ 18.swift
@@ -1,10 +1,30 @@
-func readInput() -> [String] {
-    var result: [String] = []
+struct Step {
+    let direction: Character
+    let count: Int
+    let color: String
+}
+
+func readInput() -> [Step] {
+    var result: [Step] = []
     while let line = readLine() {
-        result.append(line)
+        let splits = line.split(separator: " ")
+        let direction = splits[0].first!
+        let count = Int(splits[1])!
+        let color = String(splits[2])
+        result.append(Step(direction: direction, count: count, color: color))
     }
     return result
 }
+
+/// The area of a polygon with integral cartesian coordinates can be computed by
+/// summing up the signed areas of the trapezoids formed by consecutive pairs of
+/// vertices (Shoelace formula).
+// func area(input: String) -> Int {
+//     let (x, y) = (0, 0)
+//     for item in input {
+
+//     }
+// }
 
 let input = readInput()
 print(input)
