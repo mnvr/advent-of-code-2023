4b263b4d1aca3420fcf8b7b3eeb686d9d1a99c41
Manav <manav@mrmr.io>
2023-12-21 14:11:02 +0530

    Isolate P2 - we probably need to detect cycles

--- 20.swift
+++ 20.swift
@@ -134,7 +134,48 @@ func simulate(modules: Modules, times: Int) -> (counts: [Bool: Int], result: Int
     return (counts, result, countTillRx: countTillRx!)
 }
 
+
+func simulateP2(modules: Modules) -> Int {
+    // Examples don't have "rx", so don't go into an infinite loop.
+    if (!haveRx(modules: modules)) {
+        return 0
+    }
+
+    let buttonPress = (ping: (pulse: false, from: "button"), to: "broadcaster")
+
+    var counts = [Bool: Int]()
+    var states = [String: Module.State]()
+
+    initConjunctions(modules: modules, states: &states)
+
+    var c = 0
+    while true {
+        c += 1
+
+        var pending = [buttonPress]
+        counts[buttonPress.ping.pulse] = counts[buttonPress.ping.pulse, default: 0] + 1
+        while let e = pending.popLast() {
+            if e.to == "rx" {
+                print("at count \(c) sending \(e.ping.pulse) to rx")
+                if !e.ping.pulse {
+                    return c
+                }
+            }
+
+            let destination = modules[e.to, default: Module(name: e.to, type: nil, outputs: [])]
+            let state = states[e.to, default: Module.State()]
+            if let change = propogate(ping: e.ping, module: destination, state: state) {
+                states[e.to] = change.state
+                pending.append(contentsOf: change.emits)
+                for b in change.emits.map({ $0.ping.pulse }) {
+                    counts[b] = counts[b, default: 0] + 1
+                }
+            }
+        }
+    }
+}
+
 let modules = readInput()
-let p1 = simulate(modules: modules, times: 1000)
+let p1 = simulateP2(modules: modules)
 print(p1)
 // print(p1.result)
