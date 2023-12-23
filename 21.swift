28a1d243131a0019b861732886072d3ff7e5bf76
Manav <manav@mrmr.io>
2023-12-23 07:06:30 +0530

    Create a branching variation that gives the same result on p1 example

--- 21.swift
+++ 21.swift
@@ -86,12 +86,122 @@ extension Grid: CustomStringConvertible {
     }
 }
 
-func readInput() -> Grid {
+// MARK: P2 ---
+
+// Each cell here tracks if it is occupied in the superposition of all the
+// possible grids that are overlaid on top of each other. This is easy to do for
+// this particular problem - we can just wrap around at the bounds. This works
+// because the next step does not depend on the neighbours (like in the game of
+// life), but instead we can just propogate our current count of occupancy to
+// our neighbours.
+struct QuantumGrid {
+    var map: [[Int]]
+    let start: Coordinate
+    let max: Coordinate
+
+    init(map: Map) {
+        self.map = Self.makeCountMap(map)
+        self.start = Self.findStart(map)
+        self.max = Coordinate(x: map[0].count - 1, y: map.count - 1)
+    }
+
+    static func makeCountMap(_ map: Map) -> [[Int]] {
+        var result = [[Int]]()
+        for row in map {
+            result.append(row.map { cell in switch cell {
+                case "#": -1
+                case "S": 1
+                default: 0
+            }})
+        }
+        return result
+    }
+
+    static func findStart(_ map: Map) -> Coordinate {
+        for (y, row) in map.enumerated() {
+            for (x, c) in row.enumerated() {
+                if c == "S" { return Coordinate(x: x, y: y) }
+            }
+        }
+        fatalError()
+    }
+
+    func at(_ c: Coordinate) -> Int {
+        map[c.y][c.x]
+    }
+
+    func neighbours(of c: Coordinate) -> [Coordinate] {
+        potentialNeighbours(c).filter { at($0) >= 0 }
+    }
+
+    func wrapAtBounds(_ c: Coordinate) -> Coordinate {
+        Coordinate(x: c.x < 0 ? ((max.x + 1) + c.x) : (c.x % (max.x + 1)),
+                   y: c.y < 0 ? ((max.y + 1) + c.y) : (c.y % (max.y + 1)))
+    }
+
+    func potentialNeighbours(_ c: Coordinate) -> [Coordinate] {
+        [(c.x - 1, c.y), (c.x + 1, c.y), (c.x, c.y - 1), (c.x, c.y + 1)]
+            .map { Coordinate(x: $0, y: $1) }
+            .map { wrapAtBounds($0) }
+    }
+
+    func coordinates() -> [Coordinate] {
+        var result: [Coordinate] = []
+        for y in 0...max.y {
+            for x in 0...max.x {
+                result.append(Coordinate(x: x, y: y))
+            }
+        }
+        return result
+    }
+
+    mutating func move() {
+        func isOccupied(_ i: Int) -> Bool {
+            i > 0
+        }
+
+        let prevMap = map
+        let cs = coordinates()
+        for c in cs {
+            if isOccupied(map[c.y][c.x]) {
+                map[c.y][c.x] = 0
+            }
+        }
+        for c in cs {
+            let p = prevMap[c.y][c.x]
+            if p > 0 {
+                for n in neighbours(of: c) {
+                    map[n.y][n.x] += p
+                }
+            }
+        }
+    }
+
+    var reachable: Int {
+        map.reduce(0, { $1.reduce($0, { $0 + ($1 > 0 ? 1 : 0) })})
+    }
+}
+
+extension QuantumGrid: CustomStringConvertible {
+    var description: String {
+        func mapCell(_ i: Int) -> String {
+            String(i < 0 ? "#" : (i == 0 ? "." : "O"))
+        }
+        func mapRow(_ xs: [Int]) -> String {
+            xs.map({ mapCell($0) }).joined(separator: "")
+        }
+        return map.map { mapRow($0) }.joined(separator: "\n")
+    }
+}
+
+// MARK: Normal programming resumes
+
+func readInput() -> (Grid, QuantumGrid) {
     var map: Map = []
     while let line = readLine() {
         map.append(Array(line))
     }
-    return Grid(map: map)
+    return (Grid(map: map), QuantumGrid(map: map))
 }
 
 func bfs(grid: Grid, maxStep: Int) -> Int {
@@ -128,16 +238,31 @@ func move(grid: inout Grid, steps: Int) {
     }
 }
 
+func quantumMove(grid: inout QuantumGrid, steps: Int) {
+    for s in 1...steps {
+        grid.move()
+        if verbose > 0 {
+            print("after step \(s) we reached \(grid.reachable) cells")
+            print(grid)
+            print("")
+        }
+    }
+}
+
 let verbose = switch CommandLine.arguments.last {
     case "-v": 1
     case "-vv": 2
     default: 0
 }
 
-var grid = readInput()
+var (grid, quantumGrid) = readInput()
 move(grid: &grid, steps: 6)
 let p1 = grid.reachable
 print(p1)
+
+quantumMove(grid: &quantumGrid, steps: 6)
+let p2 = quantumGrid.reachable
+print(p2)
 
 // let c = bfs(grid: grid, maxStep: 16)
 
