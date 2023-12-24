3e5182f41623bea37d0cc1268cbc307c41e1a002
Manav <manav@mrmr.io>
2023-12-24 20:18:15 +0530

    DFS

--- 17.graph.hs
+++ 17.graph.hs
@@ -2,9 +2,11 @@
 -- example graph.
 
 import Data.Map qualified as M
+import Data.Set qualified as S
+import Data.Maybe (fromJust)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . demo . parse
+main = interact $ unlines . demo . parse
   where demo grid = dfs grid (0, 0) visitor
 
 type Node = (Int, Int)
@@ -20,5 +22,14 @@ enum = zip [0..]
 visitor :: (Show a) => (Int, Int) -> a -> String
 visitor node item = "visiting item " ++ show item ++ " at " ++ show node
 
+neighbours :: Grid a -> Node -> [Node]
+neighbours Grid { items } = filter (`M.member` items) . adjacent
+  where adjacent (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+
 dfs :: Grid a -> Node -> (Node -> a -> b) -> [b]
-dfs Grid { items, maxNode = (mx, my) } start f = []
+dfs grid@Grid { items, maxNode = (mx, my) } start visitor = go [start] S.empty
+  where
+    visit x = let item = fromJust $ M.lookup x items in visitor x item
+    go [] seen = []
+    go (x:xs) seen | S.member x seen = go xs seen
+    go (x:xs) seen = visit x : go ((neighbours grid x) ++ xs) (S.insert x seen)
