2d2b50b4e8deb2064e4bea324efb21622e2f08a2
Manav <manav@mrmr.io>
2023-12-25 18:40:38 +0530

    Add a heap
    Works correct for p1, takes 13s on full input (unoptimized)

--- 17.wip.hs
+++ 17.wip.hs
@@ -1,7 +1,6 @@
 import Data.Map qualified as M
 import Data.Set qualified as S
 import Data.Maybe (fromJust, fromMaybe, mapMaybe, listToMaybe)
-import Data.Sequence (Seq(..), fromList, (><))
 import Data.List (find, minimumBy)
 
 main :: IO ()
@@ -49,7 +48,7 @@ dijkstra :: Grid Int -> Node -> (Cell -> Bool)
             -> (Cell -> Int -> Int -> Maybe Cell -> String)
             -> (Maybe Int, [String])
 dijkstra grid@Grid { items } start isEnd visitor =
-  go (M.singleton startCell 0) (M.singleton 0 (S.fromList [startCell])) M.empty S.empty
+  go (M.singleton startCell 0) (M.singleton 0 (S.fromList [startCell])) M.empty S.empty (h_singleton (0, startCell))
   where
     -- Since moves is 0, the direction doesn't distinguish between moving ahead
     -- or turning.
@@ -58,11 +57,12 @@ dijkstra grid@Grid { items } start isEnd visitor =
                   in visitor x item d p
     -- next' ds seen = (M.lookupMin $ M.withoutKeys ds seen)
     -- next'' :: M.Map Cell Int -> S.Set Cell -> Maybe (Cell, Int)
-    next ds ids seen = case filter (\k -> S.notMember k seen) (M.keys ds) of
-      [] -> Nothing
-      ks -> let mv = minimum $ map (\k -> fromJust (M.lookup k ds)) ks
-                mk = filter (\k -> (M.lookup k ds) == (Just mv)) ks
-            in if null mk then Nothing else Just (mk !! 0, mv, ids)
+    -- next ds ids seen = case filter (\k -> S.notMember k seen) (M.keys ds) of
+    --   [] -> Nothing
+    --   ks -> let mv = minimum $ map (\k -> fromJust (M.lookup k ds)) ks
+    --             mk = filter (\k -> (M.lookup k ds) == (Just mv)) ks
+    --         in if null mk then Nothing else Just (mk !! 0, mv, ids)
+
     -- next :: M.Map Cell Int -> M.Map Int (S.Set Cell) -> S.Set Cell -> Maybe (Cell, Int, M.Map Int (S.Set Cell))
     -- next ds ids seen = case M.minView ids of
     --   Nothing -> Nothing
@@ -71,28 +71,30 @@ dijkstra grid@Grid { items } start isEnd visitor =
     --     _ -> let (u, s'') = fromJust $ S.minView s'
     --              du = fromJust $ M.lookup u ds
     --          in Just (u, du, M.insert du s'' ids')
-    go :: M.Map Cell Int -> M.Map Int (S.Set Cell) -> M.Map Cell Cell -> S.Set Cell -> (Maybe Int, [String])
-    go ds ids parent seen = case next ds ids seen of
+    go :: M.Map Cell Int -> M.Map Int (S.Set Cell) -> M.Map Cell Cell -> S.Set Cell -> Heap (Int, Cell) -> (Maybe Int, [String])
+    -- go ds ids parent seen q = case next ds ids seen of
+    go ds ids parent seen q = case extractMin q of
         Nothing -> case nearestEnd ds of
                      Nothing -> (Nothing, [])
                      Just (u, du) -> (Just du, showDistanceMap grid ds parent u)
-        Just (u, du, ids')
+        Just ((du, u), q')
           -- | isEnd u -> (Just du, [visit u] )
-          | u `S.member` seen -> error "Should've been filtered out already"
+          -- | u `S.member` seen -> error "Should've been filtered out already"
+          | u `S.member` seen -> go ds ids parent seen q'
           | otherwise ->
              let adj = (neighbours grid u)
                  adj' = filter (\Neighbour {cell} -> cell `S.notMember` seen) adj
-                 (ds', ids'', parent') = foldl (relax u du) (ds, ids', parent) adj
-                 (d', vs) = go ds' ids'' parent' (S.insert u seen)
+                 (ds', ids'', parent', q') = foldl (relax u du) (ds, ids, parent, q) adj
+                 (d', vs) = go ds' ids'' parent' (S.insert u seen) q'
             --  in (d', (visit u du (M.lookup u parent)) : vs)
              in (d', vs)
 
-    relax :: Cell -> Int -> (M.Map Cell Int, M.Map Int (S.Set Cell), M.Map Cell Cell)
-             -> Neighbour -> (M.Map Cell Int, M.Map Int (S.Set Cell), M.Map Cell Cell)
-    relax u du (ds, ids, parent) Neighbour { cell = v, distance = d } =
+    relax :: Cell -> Int -> (M.Map Cell Int, M.Map Int (S.Set Cell), M.Map Cell Cell, Heap (Int, Cell))
+             -> Neighbour -> (M.Map Cell Int, M.Map Int (S.Set Cell), M.Map Cell Cell, Heap (Int, Cell))
+    relax u du (ds, ids, parent, q) Neighbour { cell = v, distance = d } =
       case M.lookup v ds of
-        Just dv | dv < du + d -> (ds, ids, parent)
-        _ -> (M.insert v (du + d) ds, ids, M.insert v u parent)
+        Just dv | dv < du + d -> (ds, ids, parent, q)
+        _ -> (M.insert v (du + d) ds, ids, M.insert v u parent, h_insert (du + d, v) q)
 
         -- _ -> (M.insert v (du + d) ds, M.alter af (du + d) ids, M.insert v u parent)
         --     where af Nothing = Just (S.singleton v)
@@ -102,6 +104,25 @@ dijkstra grid@Grid { items } start isEnd visitor =
       [] -> Nothing
       kvs -> Just $ minimumBy (\(k, v) (k2, v2) -> v `compare` v2) kvs
 
+
+data Heap a = Empty | Heap a (Heap a) (Heap a)
+
+union :: Ord a => Heap a -> Heap a -> Heap a
+union Empty h = h
+union h Empty = h
+union hl@(Heap l ll lr) hr@(Heap r _ _)
+  | l <= r = Heap l (union hr lr) ll
+  | otherwise = union hr hl
+
+extractMin :: Ord a => Heap a -> Maybe (a, Heap a)
+extractMin Empty = Nothing
+extractMin (Heap x l r) = Just (x, union l r)
+
+h_singleton :: a -> Heap a
+h_singleton x = Heap x Empty Empty
+
+h_insert :: Ord a => a -> Heap a -> Heap a
+h_insert x h = h_singleton x `union` h
 
 showDistanceMap :: Grid a -> M.Map Cell Int -> M.Map Cell Cell -> Cell -> [String]
 showDistanceMap Grid { lastNode = (mx, my) } ds parent end = map line [0..my]
