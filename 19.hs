6c5ddd6eeea919324b341841efe07745cd409914
Manav <manav@mrmr.io>
2023-12-26 14:06:19 +0530

    Fix trailing

--- 19.hs
+++ 19.hs
@@ -12,9 +12,9 @@ parse = both . lines
   where
     both ("":ls) = ([], map part ls)
     both (l:ls) = let (w, p) = both ls in (workflow l : w, p)
-    workflow s = rules <$> break (== '{') s
+    workflow s = (rules . drop 1) <$> break (== '{') s
     rules [] = []
-    rules s = let (a, b) = break (`elem` ",}") (drop 1 s) in a : rules b
+    rules s = let (a, b) = break (`elem` ",}") s in a : rules (drop 1 b)
     part :: String -> [Int]
     part s = case reads (snd $ break isDigit s) of
         [] -> []
