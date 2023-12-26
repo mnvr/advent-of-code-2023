54439af8731e2a00edac934bb570321027bb443c
Manav <manav@mrmr.io>
2023-12-26 18:46:15 +0530

    Step on an infinite grid

--- 21.hs
+++ 21.hs
@@ -41,26 +41,24 @@ p1 garden = step 64 garden
         _ -> True
 
 p2 :: Garden -> Int
-p2 garden = p2' garden bounds
-  where bounds = maximum (M.keys garden)
+p2 garden = p2' garden 100 count
+  where count = (\(x, y) -> (x + 1, y + 1)) . maximum . M.keys $ garden
 
-p2' :: Garden -> (Index) -> Int
-p2' garden (mx, my) = trace ("bounds " ++ show (mx, my)) $ step 64 garden
+p2' :: Garden -> Int -> (Index) -> Int
+p2' garden nstep (nx, ny) = trace ("count " ++ show (nx, ny)) $ step nstep garden
   where
     step :: Int -> Garden -> Int
     step 0 g = occupied g
-    step i g = trace ("step " ++ show (64 - i) ++ " occupied " ++ show (occupied g)) $ (step (i - 1) . foldl f (blank g) . M.keys) g
+    step i g = trace ("step " ++ show (nstep - i) ++ " occupied " ++ show (occupied g)) $ (step (i - 1) . foldl f (blank g) . M.keys) g
       where f m k = case g ! k of
               'O' -> foldl (\m k' -> M.insert k' 'O' m) m (next k)
               _  -> m
-    occupied :: Garden -> Int
-    occupied = length . filter (== 'O') . M.elems
-    blank :: Garden -> Garden
-    blank = M.map (\c -> if c == 'O' then '.' else c)
     next :: Index -> [Index]
     next (x, y) = filter valid [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
     valid :: Index -> Bool
     valid k = case M.lookup k garden of
-        Nothing -> False
+        Nothing -> case M.lookup (fst k `mod` nx, snd k `mod` ny) garden of
+                     Nothing -> error ("cannot find index " ++ show (fst k `mod` nx, snd k `mod` ny))
+                     (Just c) -> c /= '#'
         (Just '#') -> False
         _ -> True
