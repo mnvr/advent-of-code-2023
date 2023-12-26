3eb2d4c3022a3a367d52d869d4c32a542863daae
Manav <manav@mrmr.io>
2023-12-26 18:35:32 +0530

    Make space for p2

--- 21.hs
+++ 21.hs
@@ -1,8 +1,9 @@
 import Data.Map qualified as M
 import Data.Map ((!))
+import Debug.Trace (trace)
 
 main :: IO ()
-main = interact $ (++ "\n") . show . p1 . parse
+main = interact $ (++ "\n") . show . p2 . parse
 
 type Index = (Int, Int)
 type Garden = M.Map Index Char
@@ -16,15 +17,44 @@ parse = foldl row M.empty . enum . lines
 enum :: [a] -> [(Int, a)]
 enum = zip [0..]
 
+occupied :: Garden -> Int
+occupied = length . filter (== 'O') . M.elems
+
+blank :: Garden -> Garden
+blank = M.map (\c -> if c == 'O' then '.' else c)
+
 p1 :: Garden -> Int
-p1 garden = step 6 garden
+p1 garden = step 64 garden
   where
     step :: Int -> Garden -> Int
-    step 0 g = (length . filter (== 'O') . M.elems) g
+    step 0 g = occupied g
     step i g = (step (i - 1) . foldl f (blank g) . M.keys) g
       where f m k = case g ! k of
               'O' -> foldl (\m k' -> M.insert k' 'O' m) m (next k)
               _  -> m
+    next :: Index -> [Index]
+    next (x, y) = filter valid [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    valid :: Index -> Bool
+    valid k = case M.lookup k garden of
+        Nothing -> False
+        (Just '#') -> False
+        _ -> True
+
+p2 :: Garden -> Int
+p2 garden = p2' garden bounds
+  where bounds = maximum (M.keys garden)
+
+p2' :: Garden -> (Index) -> Int
+p2' garden (mx, my) = trace ("bounds " ++ show (mx, my)) $ step 64 garden
+  where
+    step :: Int -> Garden -> Int
+    step 0 g = occupied g
+    step i g = trace ("step " ++ show (64 - i) ++ " occupied " ++ show (occupied g)) $ (step (i - 1) . foldl f (blank g) . M.keys) g
+      where f m k = case g ! k of
+              'O' -> foldl (\m k' -> M.insert k' 'O' m) m (next k)
+              _  -> m
+    occupied :: Garden -> Int
+    occupied = length . filter (== 'O') . M.elems
     blank :: Garden -> Garden
     blank = M.map (\c -> if c == 'O' then '.' else c)
     next :: Index -> [Index]
