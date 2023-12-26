aa20a2c736c99853705e1870a70c2f26a3dbf054
Manav <manav@mrmr.io>
2023-12-26 14:16:05 +0530

    Complete parse

--- 19.hs
+++ 19.hs
@@ -1,19 +1,19 @@
 import Data.Char (isDigit)
 import Data.List (elemIndex)
+import Data.Map qualified as M
 
 main :: IO ()
 main = interact $ (++ "\n") . show . parse
 
-type Workflow = (String, [Rule])
 type Rule = (Maybe Condition, String)
 type Condition = (Int, Char, Int)
 type Part = [Int]
 
-parse :: String -> ([Workflow], [Part])
+parse :: String -> (M.Map String [Rule], [Part])
 parse = both . lines
   where
-    both ("":ls) = ([], map part ls)
-    both (l:ls) = let (w, p) = both ls in (workflow l : w, p)
+    both s = let (a, b) = span (/= "") s
+             in (M.fromList (map workflow a), map part b)
     workflow s = (rules . drop 1) <$> break (== '{') s
     rules [] = []
     rules s = let (a, b) = break (`elem` ",}") s in rule a : rules (drop 1 b)
@@ -22,7 +22,9 @@ parse = both . lines
         (c, r) -> (Just (condition c), r)
     condition (p:c:rs) = case p `elemIndex` "xmas" of
         Just i -> (i, c, read rs)
-    part :: String -> [Int]
     part s = case reads (snd $ break isDigit s) of
         [] -> []
         [(d, r)] -> d : part r
+
+-- valid :: [Workflow] -> Part -> Bool
+-- valid ws p =
