66724a8fe2eb42c1a1853f6af18ac11ab7732670
Manav <manav@mrmr.io>
2023-12-26 14:04:58 +0530

    Recursive descent

--- 19.hs
+++ 19.hs
@@ -3,11 +3,18 @@ import Data.Char (isDigit)
 main :: IO ()
 main = interact $ (++ "\n") . show . parse
 
+type Workflow = (String, [Rule])
+type Rule = (Maybe Condition, String)
+type Condition = String -- (Int, Char, Int)
+type Part = [Int]
+
 parse = both . lines
   where
     both ("":ls) = ([], map part ls)
     both (l:ls) = let (w, p) = both ls in (workflow l : w, p)
-    workflow s = "workflow " ++ s
+    workflow s = rules <$> break (== '{') s
+    rules [] = []
+    rules s = let (a, b) = break (`elem` ",}") (drop 1 s) in a : rules b
     part :: String -> [Int]
     part s = case reads (snd $ break isDigit s) of
         [] -> []
