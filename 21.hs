3cfc378fd425fbe77013419539321bb648540aae
Manav <manav@mrmr.io>
2023-12-26 17:23:45 +0530

    Swap

--- 21.hs
+++ 21.hs
@@ -19,18 +19,18 @@ enum = zip [0..]
 p1 :: Garden -> Int
 p1 garden = step 6 garden
   where
-    blank :: Garden -> Garden
-    blank = M.map (\c -> if c == 'O' then '.' else c)
-    valid :: Index -> Bool
-    valid k = case M.lookup k garden of
-        Nothing -> False
-        (Just '#') -> False
-        _ -> True
-    next :: Index -> [Index]
-    next (x, y) = filter valid [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
     step :: Int -> Garden -> Int
     step 0 g = (length . filter (== 'O') . M.elems) g
     step i g = (step (i - 1) . foldl f (blank g) . M.keys) g
       where f m k = case g ! k of
               'O' -> foldl (\m k' -> M.insert k' 'O' m) m (next k)
               _  -> m
+    blank :: Garden -> Garden
+    blank = M.map (\c -> if c == 'O' then '.' else c)
+    next :: Index -> [Index]
+    next (x, y) = filter valid [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    valid :: Index -> Bool
+    valid k = case M.lookup k garden of
+        Nothing -> False
+        (Just '#') -> False
+        _ -> True
