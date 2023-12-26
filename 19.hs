26058ef2b096502d096345888af6c73461183798
Manav <manav@mrmr.io>
2023-12-26 13:31:10 +0530

    Scaffold 19.hs

---
+++ 19.hs
@@ -0,0 +1,9 @@
+main :: IO ()
+main = interact $ (++ "\n") . show . parse
+
+parse = both . lines
+  where
+    both ("":ls) = ([], map part ls)
+    both (l:ls) = let (w, p) = both ls in (workflow l : w, p)
+    workflow s = "workflow " ++ s
+    part s = "part " ++ s
