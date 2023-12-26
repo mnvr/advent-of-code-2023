2f35a2318d3e458a70c8be73384ad234ec3450f9
Manav <manav@mrmr.io>
2023-12-26 14:08:43 +0530

    RD 2

--- 19.hs
+++ 19.hs
@@ -1,20 +1,27 @@
 import Data.Char (isDigit)
+import Data.List (elemIndex)
 
 main :: IO ()
 main = interact $ (++ "\n") . show . parse
 
 type Workflow = (String, [Rule])
 type Rule = (Maybe Condition, String)
-type Condition = String -- (Int, Char, Int)
+type Condition = (Int, Char, Int)
 type Part = [Int]
 
+parse :: String -> ([Workflow], [Part])
 parse = both . lines
   where
     both ("":ls) = ([], map part ls)
     both (l:ls) = let (w, p) = both ls in (workflow l : w, p)
     workflow s = (rules . drop 1) <$> break (== '{') s
     rules [] = []
-    rules s = let (a, b) = break (`elem` ",}") s in a : rules (drop 1 b)
+    rules s = let (a, b) = break (`elem` ",}") s in rule a : rules (drop 1 b)
+    rule s = case break (== ':') s of
+        (r, []) -> (Nothing, r)
+        (c, r) -> (Just (condition c), r)
+    condition (p:c:rs) = case p `elemIndex` "xmas" of
+        Just i -> (i, c, read rs)
     part :: String -> [Int]
     part s = case reads (snd $ break isDigit s) of
         [] -> []
