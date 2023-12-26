2486af40d1554c7a92019467d2b96fd7f2615a72
Manav <manav@mrmr.io>
2023-12-26 07:52:01 +0530

    Sequence

--- 17.wip.swift
+++ 17.wip.swift
@@ -38,7 +38,7 @@ func shortestPath(grid: Grid, moveRange: ClosedRange<Int>) -> Int? {
     let startCell = Cell(node: startNode, direction: .l, moves: 0)
     func isEnd(_ cell: Cell) -> Bool { cell.node == grid.lastNode }
 
-    func adj(_ u: Cell) -> [Cell] {
+    func adj(_ u: Cell) -> [Neighbour] {
         neighbours(grid: grid, moveRange: moveRange, cell: u)
     }
 
@@ -63,8 +63,9 @@ func shortestPath(grid: Grid, moveRange: ClosedRange<Int>) -> Int? {
     while let (u, du) = popNearest() {
         if !seen.insert(u).inserted { continue }
         if isEnd(u) { return du }
-        for v in adj(u) {
-            let d = grid.items[v.node]!
+        for n in adj(u) {
+            let v = n.cell
+            let d = n.d
             let dv = dist[v] ?? .max
             if du + d < dv {
                 dist[v] = du + d
@@ -75,36 +76,55 @@ func shortestPath(grid: Grid, moveRange: ClosedRange<Int>) -> Int? {
     return nil
 }
 
-func neighbours(grid: Grid, moveRange: ClosedRange<Int>, cell: Cell) -> [Cell] {
+struct Neighbour {
+    let cell: Cell
+    let d: Int
+}
+
+func neighbours(grid: Grid, moveRange: ClosedRange<Int>, cell: Cell) -> [Neighbour] {
+    let node = cell.node
+    let moves = cell.moves
+    let (x, y) = (node.x, node.y)
+    let nbrs: [[Neighbour]]
+
     func make(_ xy: (Int, Int), _ direction: Direction, _ moves: Int) -> Cell {
         Cell(node: Node(x: xy.0, y: xy.1), direction: direction, moves: moves)
     }
 
-    let node = cell.node
-    let moves = cell.moves
-    let (x, y) = (node.x, node.y)
-    let cells: [Cell]
+    let cellRange = 1...moveRange.upperBound
+
+    func seq(_ makeCell: (Int) -> Cell) -> [Neighbour] {
+        var s = 0
+        return Array(cellRange.compactMap { m in
+            let cell = makeCell(m)
+            guard let d = grid.items[cell.node] else { return nil }
+            s += d
+            if moveRange.contains(cell.moves) {
+                return Neighbour(cell: cell, d: s)
+            }
+            return nil
+        })
+    }
+
     switch cell.direction {
     case .l:
-        cells = [make((x + 1, y), .l, moves + 1),
-                 make((x, y - 1), .u, 1),
-                 make((x, y + 1), .d, 1)]
+        nbrs = [seq({ m in make((x + m, y), .l, moves + m) }),
+                seq({ m in make((x, y - m), .u, m) }),
+                seq({ m in make((x, y + m), .d, m) })]
     case .r:
-        cells = [make((x - 1, y), .r, moves + 1),
-                 make((x, y - 1), .u, 1),
-                 make((x, y + 1), .d, 1)]
+        nbrs = [seq({ m in make((x - m, y), .r, moves + m) }),
+                seq({ m in make((x, y - m), .u, m) }),
+                seq({ m in make((x, y + m), .d, m) })]
     case .u:
-        cells = [make((x, y - 1), .u, moves + 1),
-                 make((x - 1, y), .r, 1),
-                 make((x + 1, y), .l, 1)]
+        nbrs = [seq({ m in make((x, y - m), .u, moves + m) }),
+                seq({ m in make((x - m, y), .r, m) }),
+                seq({ m in make((x + m, y), .l, m) })]
     case .d:
-        cells = [make((x, y + 1), .d, moves + 1),
-                 make((x - 1, y), .r, 1),
-                 make((x + 1, y), .l, 1)]
+        nbrs = [seq({ m in make((x, y + m), .d, moves + m) }),
+                seq({ m in make((x - m, y), .r, m) }),
+                seq({ m in make((x + m, y), .l, m) })]
     }
-    return cells
-      .filter { grid.items[$0.node] != nil }
-      .filter { moveRange.contains($0.moves) }
+    return nbrs.flatMap { $0 }
 }
 
 let grid = readInput()
